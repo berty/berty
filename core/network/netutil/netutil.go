2100e13d6e23474afcff947fd0d509f38e530a73
Guilhem Fanton <guilhem.fanton@gmail.com>
2018-07-30 18:39:40 +0200

    feat(network): Add stream to net.conn

--- core/network/netutil/netutil.go
+++ core/network/netutil/netutil.go
@@ -1,12 +1,97 @@
 package netutil
 
-import "net"
+import (
+	"net"
+	"time"
 
-func GetFreeTCPPort() (int, error) {
-	l, err := net.Listen("tcp", "127.0.0.1:0")
+	inet "github.com/libp2p/go-libp2p-net"
+	manet "github.com/multiformats/go-multiaddr-net"
+)
+
+// conn must implement net.conn
+var _ net.Conn = (*conn)(nil)
+
+type conn struct {
+	s          inet.Stream
+	localAddr  net.Addr
+	remoteAddr net.Addr
+}
+
+// NewConnFromStream convert a inet.Stream to a net.Conn
+func NewConnFromStream(s inet.Stream) (net.Conn, error) {
+	localAddr, err := manet.ToNetAddr(s.Conn().LocalMultiaddr())
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
-	return l.Addr().(*net.TCPAddr).Port, l.Close()
+	remoteAddr, err := manet.ToNetAddr(s.Conn().RemoteMultiaddr())
+	if err != nil {
+		return nil, err
+	}
+
+	return &conn{s, localAddr, remoteAddr}, nil
+}
+
+// Read reads data from the connection.
+// Read can be made to time out and return an Error with Timeout() == true
+// after a fixed time limit; see SetDeadline and SetReadDeadline.
+func (c *conn) Read(b []byte) (n int, err error) { // good
+	return c.s.Read(b)
+}
+
+// Close closes the connection.
+// Any blocked Read or Write operations will be unblocked and return errors.
+func (c *conn) Write(b []byte) (n int, err error) { // good
+	return c.s.Write(b)
+}
+
+// Close closes the connection.
+// Any blocked Read or Write operations will be unblocked and return errors.
+func (c *conn) Close() error { // good
+	return c.s.Reset()
+}
+
+// LocalAddr returns the local network address.
+func (c *conn) LocalAddr() net.Addr {
+	return c.localAddr
+}
+
+// RemoteAddr returns the remote network address.
+func (c *conn) RemoteAddr() net.Addr {
+	return c.remoteAddr
+}
+
+// SetDeadline sets the read and write deadlines associated
+// with the connection. It is equivalent to calling both
+// SetReadDeadline and SetWriteDeadline.
+//
+// A deadline is an absolute time after which I/O operations
+// fail with a timeout (see type Error) instead of
+// blocking. The deadline applies to all future and pending
+// I/O, not just the immediately following call to Read or
+// Write. After a deadline has been exceeded, the connection
+// can be refreshed by setting a deadline in the future.
+//
+// An idle timeout can be implemented by repeatedly extending
+// the deadline after successful Read or Write calls.
+//
+// A zero value for t means I/O operations will not time out.
+func (c *conn) SetDeadline(t time.Time) error {
+	return c.s.SetDeadline(t)
+}
+
+// SetReadDeadline sets the deadline for future Read calls
+// and any currently-blocked Read call.
+// A zero value for t means Read will not time out.
+func (c *conn) SetReadDeadline(t time.Time) error {
+	return c.s.SetReadDeadline(t)
+}
+
+// SetWriteDeadline sets the deadline for future Write calls
+// and any currently-blocked Write call.
+// Even if write times out, it may return n > 0, indicating that
+// some of the data was successfully written.
+// A zero value for t means Write will not time out.
+func (c *conn) SetWriteDeadline(t time.Time) error {
+	return c.s.SetWriteDeadline(t)
 }
