9e884b87967ee929829e5448b9c1576e728c5c04
Manfred Touron <m@42.am>
2018-08-03 08:47:11 +0200

    feat(core): implement Node.EventStream()

--- core/node/errors.go
+++ core/node/errors.go
@@ -3,8 +3,9 @@ package node
 import "errors"
 
 var (
-	ErrEntityAlreadyExists = errors.New("entity already exists")
-	ErrInvalidEventSender  = errors.New("invalid event sender")
-	ErrInvalidInput        = errors.New("invalid input")
-	ErrNotImplemented      = errors.New("not implemented")
+	ErrEntityAlreadyExists             = errors.New("entity already exists")
+	ErrInvalidEventSender              = errors.New("invalid event sender")
+	ErrInvalidInput                    = errors.New("invalid input")
+	ErrNotImplemented                  = errors.New("not implemented")
+	ErrAnotherClientIsAlreadyConnected = errors.New("another client is already connected")
 )
