0bb9ca191b961be740223fc6cceeaa7e06c6b4b7
Sacha Froment <sfroment42@gmail.com>
2018-08-14 00:51:52 +0200

    fix(web): upgrade babel version
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- client/react-native/web/scripts/test.js
+++ client/react-native/web/scripts/test.js
@@ -15,11 +15,16 @@ process.on('unhandledRejection', err => {
 // Ensure environment variables are read.
 require('../config/env');
 
+
 const jest = require('jest');
 let argv = process.argv.slice(2);
 
-// Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
+// Watch unless on CI, in coverage mode, or explicitly running all tests
+if (
+  !process.env.CI &&
+  argv.indexOf('--coverage') === -1 &&
+  argv.indexOf('--watchAll') === -1
+) {
   argv.push('--watch');
 }
 
