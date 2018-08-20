d48d2c2bb0b7a6ed6edd1656e870012c5ed699d0
aeddi <antoine.e.b@gmail.com>
2018-08-20 11:05:00 +0200

    feat: added signing function in DarwinInterface (and verifying function for ECC testing)

--- core/crypto/enclave/platform_api/darwin/DarwinInterface.m
+++ core/crypto/enclave/platform_api/darwin/DarwinInterface.m
@@ -15,6 +15,9 @@
 // Return encryption algorithm corresponding value in Darwin API
 #define encAlgoType(type) (type == ECC256) ? kSecKeyAlgorithmECIESEncryptionStandardX963SHA512AESGCM : kSecKeyAlgorithmRSAEncryptionOAEPSHA512
 
+// Return signing algorithm corresponding value in Darwin API
+#define signAlgoType(type) (type == ECC256) ? kSecKeyAlgorithmECDSASignatureMessageX962SHA512 : kSecKeyAlgorithmRSASignatureMessagePSSSHA512
+
 // Generate a key pair and store the private key in the Secure Enclave.
 // Stores only 256-bit elliptic curve private keys (no compatible with RSA).
 // This Darwin feature is only available on specific hardware:
@@ -191,6 +194,43 @@ SecKeyRef getPublicKeyFromKeychain(const char *goKeyID) {
     return publicKeyRef;
 }
 
+// Decrypt ciphertext using private key and corresponding encryption algorithm (RSA or ECC)
+NSData *decryptCiphertextUsingPrivateKey(const char *goKeyID, NSData *ciphertext, int type) {
+    NSData *plaintext = NULL;
+    CFErrorRef error = NULL;
+    SecKeyRef privateKeyRef = getPrivateKeyFromKeychain(goKeyID);
+    if (privateKeyRef) {
+        bool canDecrypt = SecKeyIsAlgorithmSupported(privateKeyRef,
+                                                     kSecKeyOperationTypeDecrypt,
+                                                     encAlgoType(type));
+        if (type == RSA2048 && [ciphertext length] != SecKeyGetBlockSize(privateKeyRef)) {
+            NSLog(@"Error during text decryption using key with id %s: for RSA-2048/SHA512 ciphertext length must be equal to Key Block Size\n", goKeyID);
+        } else if (canDecrypt) {
+            plaintext = (__bridge NSData*)(SecKeyCreateDecryptedData(
+                              privateKeyRef,
+                              encAlgoType(type),
+                              (__bridge CFDataRef)ciphertext,
+                              &error)
+                          );
+
+
+            if (!plaintext) {
+                NSLog(@"Error during text decryption using key with id %s: %@\n", goKeyID, error);
+                CFRelease(error);
+            }
+        } else {
+            NSLog(@"Error during text decryption using key with id %s: wrong algorithm\n", goKeyID);
+        }
+
+        CFRelease(privateKeyRef);
+    } else {
+        NSLog(@"Can't find public key with id %s\n", goKeyID);
+    }
+
+    return plaintext;
+}
+
+// TMP function for testing purpose
 // Encrypt plaintext using public key and corresponding encryption algorithm (RSA or ECC)
 NSData *encryptPlaintextUsingPublicKey(const char *goKeyID, const char *goPlaintext, int type) {
     NSString *plaintext = [NSString stringWithUTF8String:goPlaintext];
@@ -230,38 +270,69 @@ NSData *encryptPlaintextUsingPublicKey(const char *goKeyID, const char *goPlaint
     return ciphertext;
 }
 
-// Decrypt ciphertext using private key and corresponding encryption algorithm (RSA or ECC)
-NSData *decryptCiphertextUsingPrivateKey(const char *goKeyID, NSData *ciphertext, int type) {
-    NSData *plaintext = NULL;
-    CFErrorRef error = NULL;
-    SecKeyRef privateKeyRef = getPrivateKeyFromKeychain(goKeyID);
+// Sign data using private key and corresponding signing algorithm (RSA or ECC)
+NSData *signDataUsingPrivateKey(const char *goKeyID, NSData *data, int type) {
+  	NSData* signature = NULL;
+  	CFErrorRef error = NULL;
+  	SecKeyRef privateKeyRef = getPrivateKeyFromKeychain(goKeyID);
     if (privateKeyRef) {
-        bool canDecrypt = SecKeyIsAlgorithmSupported(privateKeyRef,
-                                                     kSecKeyOperationTypeDecrypt,
-                                                     encAlgoType(type));
-        if (type == RSA2048 && [ciphertext length] != SecKeyGetBlockSize(privateKeyRef)) {
-            NSLog(@"Error during text decryption using key with id %s: for RSA-2048/SHA512 ciphertext length must be equal to Key Block Size\n", goKeyID);
-        } else if (canDecrypt) {
-            plaintext = (__bridge NSData*)(SecKeyCreateDecryptedData(
-                              privateKeyRef,
-                              encAlgoType(type),
-                              (__bridge CFDataRef)ciphertext,
-                              &error)
-                          );
+        bool canSign = SecKeyIsAlgorithmSupported(privateKeyRef,
+                                                  kSecKeyOperationTypeSign,
+                                                  signAlgoType(type));
+        if (canSign) {
+          	signature = (__bridge NSData*)(SecKeyCreateSignature(
+                  					privateKeyRef,
+                            signAlgoType(type),
+                  					(__bridge CFDataRef)data,
+                  					&error)
+                				);
 
-
-            if (!plaintext) {
-                NSLog(@"Error during text decryption using key with id %s: %@\n", goKeyID, error);
-                CFRelease(error);
+          	if (!signature) {
+            		NSLog(@"Error during signature generation using key with id %s: %@\n", goKeyID, error);
+            		CFRelease(error);
             }
         } else {
-            NSLog(@"Error during text decryption using key with id %s: wrong algorithm\n", goKeyID);
+              NSLog(@"Error during signature generation using key with id %s: wrong algorithm\n", goKeyID);
         }
 
-        CFRelease(privateKeyRef);
+      	CFRelease(privateKeyRef);
+    } else {
+        NSLog(@"Can't find private key with id %s\n", goKeyID);
+    }
+
+  	return signature;
+}
+
+// TMP function for testing purpose
+// Verify signature using public key and corresponding signing algorithm (RSA or ECC)
+bool verifySignatureUsingPublicKey(const char *goKeyID, NSData *data, NSData *signature, int type) {
+    bool verification = false;
+  	CFErrorRef error = NULL;
+  	SecKeyRef publicKeyRef = getPublicKeyFromKeychain(goKeyID);
+    if (publicKeyRef) {
+      	bool canVerify = SecKeyIsAlgorithmSupported(publicKeyRef,
+                                										kSecKeyOperationTypeVerify,
+                                										signAlgoType(type));
+        if (canVerify) {
+          	verification = SecKeyVerifySignature(
+                      							publicKeyRef,
+                      							signAlgoType(type),
+                      							(__bridge CFDataRef)signature,
+                      							(__bridge CFDataRef)data,
+                      							&error
+                    						);
+          	if (!verification) {
+            		NSLog(@"Error during signature verification using key with id %s: %@\n", goKeyID, error);
+            		CFRelease(error);
+          	}
+        } else {
+            NSLog(@"Error during signature verification using key with id %s: wrong algorithm\n", goKeyID);
+        }
+
+      	CFRelease(publicKeyRef);
     } else {
         NSLog(@"Can't find public key with id %s\n", goKeyID);
     }
 
-    return plaintext;
+  	return verification;
 }
