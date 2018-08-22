b2a1cfb6e703721fee8b8887d270b588f5402b8a
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2018-08-22 17:54:10 +0200

    feat(rn): setup relay subscriptions
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- client/react-native/common/relay.js
+++ client/react-native/common/relay.js
@@ -1,4 +1,5 @@
 import { Environment, Network, RecordSource, Store } from 'relay-runtime'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
 import fetch from 'isomorphic-fetch'
 
 // Define a function that fetches the results of an operation (query/mutation/etc)
@@ -22,8 +23,23 @@ const fetchQuery = async (operation, variables) => {
   }
 }
 
+// eslint-disable-next-line
+const setupSubscription = (config, variables, cacheConfig, observer) => {
+  const query = config.text
+
+  const subscriptionClient = new SubscriptionClient('ws://localhost:8700', {
+    reconnect: true,
+  })
+  subscriptionClient.subscribe({ query, variables }, (error, result) => {
+    if (error != null) {
+      console.error(error)
+    }
+    observer.onNext({ data: result })
+  })
+}
+
 // Create a network layer from the fetch function
-const network = Network.create(fetchQuery)
+const network = Network.create(fetchQuery, setupSubscription)
 const store = new Store(new RecordSource())
 
 const environment = new Environment({
