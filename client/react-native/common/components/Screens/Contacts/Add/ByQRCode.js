17f00eecdcf28a675e55d8b6c86afaa0a066a024
aeddi <antoine.e.b@gmail.com>
2018-09-10 17:21:58 +0200

    feat(rn): added Choice screen, added Request and RequestValidation and improved QR Code screen

--- client/react-native/common/components/Screens/Contacts/Add/ByQRCode.js
+++ client/react-native/common/components/Screens/Contacts/Add/ByQRCode.js
@@ -1,7 +1,41 @@
 import { PureComponent } from 'react'
+import createTabNavigator from 'react-navigation-deprecated-tab-navigator/src/createTabNavigator'
+import { colors } from '../../../../constants'
+import {
+  borderBottom,
+} from '../../../../styles'
 
-export default class ByQRCode extends PureComponent {
+class ByQRCode extends PureComponent {
   render () {
     return null
   }
 }
+
+export default createTabNavigator(
+  {
+    'Scan a QR code': ByQRCode,
+    'View my QR code': ByQRCode,
+  },
+  {
+    initialRouteName: 'Scan a QR code',
+    swipeEnabled: true,
+    animationEnabled: true,
+    tabBarPosition: 'top',
+
+    tabBarOptions: {
+      labelStyle: {
+        color: colors.black,
+      },
+      indicatorStyle: {
+        backgroundColor: colors.black,
+      },
+      style: [
+        {
+          backgroundColor: colors.white,
+          borderTopWidth: 0,
+        },
+        borderBottom,
+      ],
+    },
+  }
+)
