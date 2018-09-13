54fb2ad5b2d89f557ede82634920e268169fc3b1
Manfred Touron <m@42.am>
2018-09-13 18:03:32 +0200

    chore: switch to moul.io/protoc-gen-gotemplate (canonical url)

--- core/entity/conversation.pb.go
+++ core/entity/conversation.pb.go
@@ -439,6 +439,9 @@ func encodeVarintConversation(dAtA []byte, offset int, v uint64) int {
 	return offset + 1
 }
 func (m *Conversation) Size() (n int) {
+	if m == nil {
+		return 0
+	}
 	var l int
 	_ = l
 	l = len(m.ID)
@@ -474,6 +477,9 @@ func (m *Conversation) Size() (n int) {
 }
 
 func (m *ConversationMember) Size() (n int) {
+	if m == nil {
+		return 0
+	}
 	var l int
 	_ = l
 	l = len(m.ID)
