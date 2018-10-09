f126c6bda8bc924a24fef5164fc888dca6be79a4
aeddi <antoine.e.b@gmail.com>
2018-10-09 23:23:16 +0200

    refactor(rn): refactor react-native folder and fix build issues

--- client/react-native/common/registerServiceWorker.js
+++ client/react-native/common/registerServiceWorker.js
@@ -8,6 +8,10 @@
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
 
+/**
+ * @jest-environment jsdom
+ */
+
 import fetch from 'whatwg-fetch'
 
 const isLocalhost = Boolean(
