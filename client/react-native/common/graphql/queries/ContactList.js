4bcc7533d16d9b1ec681898dfb3c50cbbabbacd5
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2018-10-22 18:59:06 +0200

    fix(rn): pending request pagination
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- client/react-native/common/graphql/queries/ContactList.js
+++ client/react-native/common/graphql/queries/ContactList.js
@@ -1,6 +1,6 @@
 import { graphql } from 'react-relay'
 
-export default graphql`
+const ContactList = graphql`
   query ContactListQuery(
     $filter: BertyEntityContactInput
     $count: Int32
@@ -9,3 +9,53 @@ export default graphql`
     ...ContactList @arguments(filter: $filter, count: $count, cursor: $cursor)
   }
 `
+
+ContactList.Received = graphql`
+  query ContactListReceivedQuery(
+    $filter: BertyEntityContactInput
+    $count: Int32
+    $cursor: String
+  ) {
+    ...ContactListReceived
+      @arguments(filter: $filter, count: $count, cursor: $cursor)
+  }
+`
+
+ContactList.Received.defaultVariables = {
+  filter: {
+    id: '',
+    status: 4,
+    displayName: '',
+    displayStatus: '',
+    overrideDisplayName: '',
+    overrideDisplayStatus: '',
+  },
+  count: 10,
+  cursor: '',
+}
+
+ContactList.Sent = graphql`
+  query ContactListSentQuery(
+    $filter: BertyEntityContactInput
+    $count: Int32
+    $cursor: String
+  ) {
+    ...ContactListSent
+      @arguments(filter: $filter, count: $count, cursor: $cursor)
+  }
+`
+
+ContactList.Sent.defaultVariables = {
+  filter: {
+    id: '',
+    status: 3,
+    displayName: '',
+    displayStatus: '',
+    overrideDisplayName: '',
+    overrideDisplayStatus: '',
+  },
+  count: 10,
+  cursor: '',
+}
+
+export default ContactList
