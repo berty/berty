dfc3b82363650c7460067328c42bfcc7fac82462
Sacha Froment <sfroment42@gmail.com>
2018-10-23 15:40:09 +0200

    feat(core): add error message on failt connection
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- core/manager/account/account.go
+++ core/manager/account/account.go
@@ -79,6 +79,8 @@ func (a *Account) Validate() error {
 		return errors.New("missing required field (Passphrase) for account")
 	} else if a.dbPath == "" {
 		return errors.New("missing required field (dbPath) for account")
+	} else if a.db == nil {
+		return errors.New("connecting to the db failed with the provided (dbPath/Passphrase) for account")
 	} else if a.network == nil {
 		return errors.New("missing required field (network) for account")
 	} else if a.grpcServer == nil {
