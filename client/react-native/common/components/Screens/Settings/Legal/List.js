b57152b3a3f6f17a0eb8016c2f3667e88eaddeed
aeddi <antoine.e.b@gmail.com>
2018-10-24 18:57:41 +0200

    improve(rn): images made responsive on about, help and legal screens

--- client/react-native/common/components/Screens/Settings/Legal/List.js
+++ client/react-native/common/components/Screens/Settings/Legal/List.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
-import { Image } from 'react-native'
-import { Header, Menu, Flex } from '../../../Library'
+import { View, Image } from 'react-native'
+import { Header, Menu } from '../../../Library'
 
 export default class List extends PureComponent {
   static navigationOptions = ({ navigation }) => ({
@@ -11,41 +11,39 @@ export default class List extends PureComponent {
   render () {
     const { navigation } = this.props
     return (
-      <Menu style={{ marginTop: 42 }}>
-        <Flex.Cols size={1} align='center' justify='between'>
-          <Flex.Rows size={1} align='center' justify='between'>
-            <Image
-              resizeMode='contain'
-              style={{ width: 300, height: 300 }}
-              source={require('../../../../static/img/square_legal.png')}
+      <View style={{ flex: 1 }}>
+        <Image
+          resizeMode='contain'
+          style={{ flex: 3, width: null, height: null, marginTop: 42 }}
+          source={require('../../../../static/img/square_legal.png')}
+        />
+        <Menu>
+          <Menu.Section>
+            <Menu.Item
+              icon='book-open'
+              title='Privacy policy'
+              onPress={() => navigation.push('legal/privacy')}
             />
-          </Flex.Rows>
-        </Flex.Cols>
-        <Menu.Section>
-          <Menu.Item
-            icon='book-open'
-            title='Privacy policy'
-            onPress={() => navigation.push('legal/privacy')}
-          />
-          <Menu.Item
-            icon='book-open'
-            title='Terms of service'
-            onPress={() => navigation.push('legal/terms')}
-          />
-        </Menu.Section>
-        <Menu.Section>
-          <Menu.Item
-            icon='layers'
-            title='App credits'
-            onPress={() => navigation.push('legal/credits')}
-          />
-          <Menu.Item
-            icon='layers'
-            title='Software license'
-            onPress={() => navigation.push('legal/license')}
-          />
-        </Menu.Section>
-      </Menu>
+            <Menu.Item
+              icon='book-open'
+              title='Terms of service'
+              onPress={() => navigation.push('legal/terms')}
+            />
+          </Menu.Section>
+          <Menu.Section>
+            <Menu.Item
+              icon='layers'
+              title='App credits'
+              onPress={() => navigation.push('legal/credits')}
+            />
+            <Menu.Item
+              icon='layers'
+              title='Software license'
+              onPress={() => navigation.push('legal/license')}
+            />
+          </Menu.Section>
+        </Menu>
+      </View>
     )
   }
 }
