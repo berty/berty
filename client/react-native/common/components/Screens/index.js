84cf9af2bf3c2a7dbf6e1b92c3e1a46c3127c4fe
Guillaume Louvigny <github@ohai.fr>
2018-10-24 18:31:59 +0200

    feat(ios): berty: deep link to add a contact

--- client/react-native/common/components/Screens/index.js
+++ client/react-native/common/components/Screens/index.js
@@ -1,11 +1,14 @@
 import createTabNavigator from 'react-navigation-deprecated-tab-navigator/src/createTabNavigator'
+import { Animated, Easing } from 'react-native'
+import { createStackNavigator } from 'react-navigation'
 import Contacts from './Contacts'
 import Chats from './Chats'
 import Settings from './Settings'
 import { colors } from '../../constants'
 import { borderTop, shadow } from '../../styles'
+import { ByPublicKeyModal } from './Contacts/Add/ByPublicKey'
 
-export default createTabNavigator(
+export const mainTabs = createTabNavigator(
   {
     contacts: Contacts,
     chats: Chats,
@@ -31,5 +34,54 @@ export default createTabNavigator(
         shadow,
       ],
     },
-  }
+  },
+)
+
+// Navigator handling modals
+export default createStackNavigator(
+  {
+    'main': {
+      screen: mainTabs,
+    },
+    'modal/contacts/add/by-public-key': {
+      screen: ByPublicKeyModal,
+    },
+  },
+  {
+    mode: 'card',
+    headerMode: 'none',
+    transparentCard: true,
+    cardStyle: [
+      {
+        backgroundColor: colors.transparentGrey,
+      },
+    ],
+    navigationOptions: {
+      gesturesEnabled: false,
+    },
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 300,
+        easing: Easing.out(Easing.poly(4)),
+        timing: Animated.timing,
+      },
+      screenInterpolator: sceneProps => {
+        const { layout, position, scene } = sceneProps
+        const { index } = scene
+
+        const height = layout.initHeight
+        const translateY = position.interpolate({
+          inputRange: [index - 1, index, index + 1],
+          outputRange: [height, 0, 0],
+        })
+
+        const opacity = position.interpolate({
+          inputRange: [index - 1, index - 0.99, index],
+          outputRange: [0, 1, 1],
+        })
+
+        return { opacity, transform: [{ translateY }] }
+      },
+    }),
+  },
 )
