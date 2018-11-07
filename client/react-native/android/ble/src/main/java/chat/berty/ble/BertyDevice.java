45c95e5846f5f8a9dddae57edf03c66e96aaf3e7
Sacha Froment <sfroment42@gmail.com>
2018-11-07 11:16:07 +0100

    feat(java): populate all charact
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- client/react-native/android/ble/src/main/java/chat/berty/ble/BertyDevice.java
+++ client/react-native/android/ble/src/main/java/chat/berty/ble/BertyDevice.java
@@ -2,6 +2,7 @@ package chat.berty.ble;
 
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
 
 public class BertyDevice {
 
@@ -9,6 +10,13 @@ public class BertyDevice {
     public String peerID;
     public String ma;
     public long mtu;
+
+    protected BluetoothGattCharacteristic acceptCharacteristic;
+    protected BluetoothGattCharacteristic maCharacteristic;
+    protected BluetoothGattCharacteristic peerIDCharacteristic;
+    protected BluetoothGattCharacteristic writerCharacteristic;
+    protected BluetoothGattCharacteristic isRdyCharacteristic;
+    protected BluetoothGattCharacteristic closerCharacteristic;
 
     public BluetoothGatt gatt;
 
