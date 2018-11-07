cc9b72c40579217e749bbf9a89f99ede58a1a161
aeddi <antoine.e.b@gmail.com>
2018-11-07 18:31:38 +0100

    feat(devtools): added go functions to get bot running state, start and stop bot

--- core/manager/account/account.go
+++ core/manager/account/account.go
@@ -49,7 +49,9 @@ type Account struct {
 
 	node     *node.Node
 	initOnly bool
-	withBot  bool
+
+	withBot    bool
+	BotRunning bool
 
 	serverTracerCloser io.Closer
 	dialTracerCloser   io.Closer
@@ -148,7 +150,7 @@ func (a *Account) Open() error {
 		return err
 	}
 	if a.withBot {
-		if err := a.startBot(); err != nil {
+		if err := a.StartBot(); err != nil {
 			return err
 		}
 	}
@@ -189,7 +191,9 @@ func (a *Account) Close() {
 	if a.grpcListener != nil {
 		a.grpcListener.Close()
 	}
-
+	if a.BotRunning {
+		_ = a.StopBot()
+	}
 }
 
 // Database
@@ -262,7 +266,7 @@ func (a *Account) startGrpcServer() error {
 	return nil
 }
 
-func (a *Account) startBot() error {
+func (a *Account) StartBot() error {
 	options := append(
 		[]bot.Option{
 			bot.WithTCPDaemon(a.GrpcBind),
@@ -274,17 +278,25 @@ func (a *Account) startBot() error {
 	if err != nil {
 		return errors.Wrap(err, "failed to initialize bot")
 	}
+	a.BotRunning = true
 
 	go func() {
 		logger().Debug("starting bot...")
 		if err := b.Start(); err != nil {
 			logger().Error("bot error", zap.Error(err))
+			a.BotRunning = false
 			defer a.PanicHandler()
 			a.errChan <- err
 		}
 	}()
 
 	return nil
+}
+
+func (a *Account) StopBot() error {
+	// TODO: implement bot closing function
+	// Then set a.BotRunning = false
+	return errors.New("stop bot not implemented yet")
 }
 
 func (a *Account) startGQL() error {
