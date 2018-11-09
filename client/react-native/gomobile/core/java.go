c93efd5b14b26f921578d47f3df1452bf217b14b
Sacha Froment <sfroment42@gmail.com>
2018-11-09 15:46:56 +0100

    chore(merge): fix bad merge resolve
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- client/react-native/gomobile/core/java.go
+++ client/react-native/gomobile/core/java.go
@@ -19,14 +19,6 @@ func initBleFunc() {
 	ble.DialPeer = Manager.GetInstance().DialPeer
 }
 
-func JavaExportTestFunc() {
-	fmt.Printf("LALALALLA %+v\n\n\n", Manager.GetInstance())
-}
-
-func JavaCallTestFunc() {
-	fmt.Printf("REAL SHIT %+v\n\n\n\n\n", Manager.GetInstance().RealTest())
-}
-
 func JavaToGo() string {
 	return "COMING FROM GOLANG"
 }
