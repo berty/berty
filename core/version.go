80b1593253f6c24f9780a9599476c4b6c53e895b
Manfred Touron <m@42.am>
2018-11-21 17:14:14 +0100

    feat: return commit date in device infos

--- core/version.go
+++ core/version.go
@@ -1,13 +1,32 @@
 package core // import "berty.tech/core"
 
+import (
+	"strconv"
+	"time"
+)
+
 var (
 	// Version is the core semver version
 	Version = "1.0.0"
 
 	// the following variables are updated at build phase using `go build -X`
 
-	GitSha    = "undefined"
-	GitTag    = "undefined"
-	GitBranch = "undefined"
-	BuildMode = "undefined"
+	GitSha     = "undefined"
+	GitTag     = "undefined"
+	GitBranch  = "undefined"
+	BuildMode  = "undefined"
+	commitDate = "undefined"
 )
+
+func CommitDate() string {
+	if commitDate == "undefined" {
+		return "undefined"
+	}
+
+	commitDateInt, err := strconv.ParseInt(commitDate, 10, 64)
+	if err != nil {
+		return "parse-error"
+	}
+	tm := time.Unix(commitDateInt, 0)
+	return tm.Format(time.RFC3339)
+}
