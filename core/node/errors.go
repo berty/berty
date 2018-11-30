1e129667d5e7bce99b7441a77d70f0c0e09f7ba9
Guillaume Louvigny <github@ohai.fr>
2018-11-30 15:14:47 +0100

    feat(core): better contact request management

--- core/node/errors.go
+++ core/node/errors.go
@@ -3,6 +3,7 @@ package node
 import "errors"
 
 var (
+	ErrContactIsMyself                 = errors.New("contact is myself")
 	ErrEntityAlreadyExists             = errors.New("entity already exists")
 	ErrInvalidEventSender              = errors.New("invalid event sender")
 	ErrInvalidInput                    = errors.New("invalid input")
