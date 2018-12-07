fd32afb9bfaf18b1293ab4d48d4a54cb7fcda2b0
Manfred Touron <m@42.am>
2018-12-07 16:52:06 +0100

    chore: move yolo code in pkg/yolo

--- core/cmd/berty/update.go
+++ core/cmd/berty/update.go
@@ -3,16 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
 
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 
 	"berty.tech/core"
+	"berty.tech/core/pkg/yolo"
 )
 
 type updateOptions struct {
@@ -39,41 +36,13 @@ func newUpdateCommand() *cobra.Command {
 }
 
 func update(opts *updateOptions) error {
-	client := http.Client{}
-	req, err := http.NewRequest(http.MethodGet, "https://yolo.berty.io/release/ios.json", nil)
+	release, err := yolo.LatestIOSMaster()
 	if err != nil {
-		return errors.Wrap(err, "failed to create http request")
-	}
-	resp, err := client.Do(req)
-	if resp != nil {
-		defer resp.Body.Close()
-	}
-	if err != nil {
-		return errors.Wrap(err, "failed to fetch release file")
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return errors.Wrap(err, "failed to read body")
-	}
-	type release struct {
-		Branch      string    `json:"branch"`
-		StopTime    time.Time `json:"stop-time"`
-		Author      string    `json:"author"`
-		GitSha      string    `json:"git-sha"`
-		BuildURL    string    `json:"build-url"`
-		Body        string    `json:"body"`
-		ManifestURL string    `json:"manifest-url"`
-	}
-	ret := struct {
-		Master    *release   `json:"master"`
-		LatestPRs []*release `json:"latest-prs"`
-	}{}
-	if err := json.Unmarshal(body, &ret); err != nil {
-		return errors.Wrap(err, "invalid json body")
+		return err
 	}
 
-	if core.CommitDate().Before(ret.Master.StopTime) {
-		out, _ := json.MarshalIndent(ret.Master, "", "  ")
+	if release.IsMoreRecentThan(core.CommitDate()) {
+		out, _ := json.MarshalIndent(release, "", "  ")
 		fmt.Printf("new master update available: %s\n", string(out))
 	} else {
 		fmt.Println("you are already up to date")
