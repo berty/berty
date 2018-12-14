c9ecfaa08336d2801b7f27198e86bf06c90c7386
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2018-12-14 16:38:33 +0100

    fix(rn): fix android conversation text input
    fix #744
    
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- client/react-native/common/components/Screens/Chats/Detail.js
+++ client/react-native/common/components/Screens/Chats/Detail.js
@@ -1,4 +1,4 @@
-import { Platform, View } from 'react-native'
+import { Platform, TextInput as RNTextInput } from 'react-native'
 import React, { PureComponent } from 'react'
 
 import { Pagination, RelayContext } from '../../../relay'
@@ -6,9 +6,9 @@ import { Text, Flex, Screen, Header, Icon } from '../../Library'
 import { colors } from '../../../constants'
 import { fragments } from '../../../graphql'
 import { merge } from '../../../helpers'
+import { parseEmbedded } from '../../../helpers/json'
 import { shadow } from '../../../styles'
 import { conversation as utils } from '../../../utils'
-import { parseEmbedded } from '../../../helpers/json'
 
 class Message extends React.PureComponent {
   static contextType = RelayContext
@@ -86,12 +86,49 @@ class Message extends React.PureComponent {
 
 const MessageContainer = fragments.Event(Message)
 
+class TextInput extends PureComponent {
+  state = {
+    height: 16,
+    value: '',
+  }
+
+  onContentSizeChange = ({
+    nativeEvent: {
+      contentSize: { height },
+    },
+  }) => this.setState({ height: height > 80 ? 80 : height })
+
+  render () {
+    const { height } = this.state
+    const { value } = this.props
+    return (
+      <RNTextInput
+        style={[
+          {
+            flex: 1,
+            padding: 0,
+            marginVertical: 8,
+            marginHorizontal: 0,
+            height: height,
+          },
+          Platform.OS === 'web' ? { paddingLeft: 16 } : {},
+        ]}
+        onContentSizeChange={this.onContentSizeChange}
+        autoFocus
+        placeholder='Write a secure message...'
+        onChangeText={this.props.onChangeText}
+        value={value}
+        multiline
+      />
+    )
+  }
+}
+
 class Input extends PureComponent {
   static contextType = RelayContext
 
   state = {
     input: '',
-    height: 16,
   }
 
   async componentDidMount () {
@@ -126,57 +163,48 @@ class Input extends PureComponent {
       }
     })
   }
+  onChangeText = value => this.setState({ input: value })
 
   render () {
     return (
-      <Flex.Cols style={[shadow]} justify='center' align='center'>
+      <Flex.Cols
+        size={0}
+        justify='center'
+        align='center'
+        style={
+          Platform.OS === 'web'
+            ? [{ position: 'absolute', bottom: 0, left: 0, right: 0 }, shadow]
+            : [shadow]
+        }
+      >
         <Flex.Cols
           style={{
             backgroundColor: colors.grey8,
-            marginLeft: 16,
+            marginLeft: 8,
+            marginRight: 3,
             borderRadius: 16,
+            marginVertical: 8,
           }}
         >
           <Text
             left
-            middle
-            margin={{
-              left: 8,
-              right: Platform.OS === 'web' ? 8 : 0,
-              top: this.state.height > 20 ? 0 : 8,
-              bottom: this.state.height > 20 ? 0 : 8,
-            }}
-            lineHeight={this.state.height > 20 ? 0 : 16}
-            background={colors.grey8}
+            top
+            size={0}
             icon='edit-2'
-            height={this.state.height}
-            input={{
-              onChangeText: input => {
-                this.setState({ input })
-              },
-              onContentSizeChange: ({
-                nativeEvent: {
-                  contentSize: { height },
-                },
-              }) => {
-                this.setState({
-                  height: height > 80 ? 80 : height,
-                })
-              },
-              placeholder: 'Write a secure message...',
-              autoFocus: true,
-              value: this.state.input,
-            }}
-            multiline
-            color={colors.grey5}
+            padding={{ right: 5, left: 8, vertical: 7 }}
+          />
+          <TextInput
+            onChangeText={this.onChangeText}
+            value={this.state.input}
           />
         </Flex.Cols>
         <Text
           right
           size={0}
           middle
-          margin
-          height={this.state.height}
+          margin={{ right: 8, ...(Platform.OS === 'web' ? { left: 12 } : {}) }}
+          padding
+          large
           icon='send'
           color={colors.grey5}
           onPress={this.onSubmit}
@@ -214,45 +242,40 @@ export default class Detail extends PureComponent {
 
     return (
       <Screen style={{ backgroundColor: colors.white, paddingTop: 0 }}>
-        <Pagination
-          direction='forward'
-          query={queries.EventList.graphql}
-          variables={merge([
-            queries.EventList.defaultVariables,
-            {
-              filter: {
-                kind: 302,
-                conversationId: conversation.id,
+        <Flex.Rows>
+          <Pagination
+            style={[
+              { flex: 1 },
+              Platform.OS === 'web' ? { paddingTop: 48 } : {},
+            ]}
+            direction='forward'
+            query={queries.EventList.graphql}
+            variables={merge([
+              queries.EventList.defaultVariables,
+              {
+                filter: {
+                  kind: 302,
+                  conversationId: conversation.id,
+                },
               },
-            },
-          ])}
-          subscriptions={[subscriptions.newMessage]}
-          fragment={fragments.EventList}
-          alias='EventList'
-          renderItem={props => (
-            <MessageContainer
-              {...props}
-              navigation={navigation}
-              screenProps={this.props.screenProps}
-            />
-          )}
-          inverted
-          style={{ paddingTop: 48 }}
-        />
-        <View
-          style={{
-            left: 0,
-            right: 0,
-            bottom: 0,
-            position: 'absolute',
-            backgroundColor: colors.white,
-          }}
-        >
+            ])}
+            subscriptions={[subscriptions.newMessage]}
+            fragment={fragments.EventList}
+            alias='EventList'
+            renderItem={props => (
+              <MessageContainer
+                {...props}
+                navigation={navigation}
+                screenProps={this.props.screenProps}
+              />
+            )}
+            inverted
+          />
           <Input
             navigation={this.props.navigation}
             screenProps={this.props.screenProps}
           />
-        </View>
+        </Flex.Rows>
       </Screen>
     )
   }
