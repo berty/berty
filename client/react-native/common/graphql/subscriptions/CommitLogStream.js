ef5c381d969e6871904d6d343a49898f1cd3388e
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2018-12-20 11:27:08 +0100

    fix(relay): add commit logs subscription
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

---
+++ client/react-native/common/graphql/subscriptions/CommitLogStream.js
@@ -0,0 +1,146 @@
+import { graphql } from 'react-relay'
+
+import { subscriber } from '../../relay'
+
+const CommitLogStream = graphql`
+  subscription CommitLogStreamSubscription {
+    CommitLogStream(T: true) {
+      operation
+      entity {
+        contact {
+          id
+          createdAt
+          updatedAt
+          sigchain
+          status
+          devices {
+            id
+            createdAt
+            updatedAt
+            name
+            status
+            apiVersion
+            contactId
+          }
+          displayName
+          displayStatus
+          overrideDisplayName
+          overrideDisplayStatus
+        }
+        device {
+          id
+          createdAt
+          updatedAt
+          name
+          status
+          apiVersion
+          contactId
+        }
+        conversation {
+          id
+          createdAt
+          updatedAt
+          readAt
+          title
+          topic
+          members {
+            id
+            createdAt
+            updatedAt
+            status
+            contact {
+              id
+              createdAt
+              updatedAt
+              sigchain
+              status
+              devices {
+                id
+                createdAt
+                updatedAt
+                name
+                status
+                apiVersion
+                contactId
+              }
+              displayName
+              displayStatus
+              overrideDisplayName
+              overrideDisplayStatus
+            }
+            conversationId
+            contactId
+          }
+        }
+        conversationMember {
+          id
+          createdAt
+          updatedAt
+          status
+          contact {
+            id
+            createdAt
+            updatedAt
+            sigchain
+            status
+            devices {
+              id
+              createdAt
+              updatedAt
+              name
+              status
+              apiVersion
+              contactId
+            }
+            displayName
+            displayStatus
+            overrideDisplayName
+            overrideDisplayStatus
+          }
+        }
+        event {
+          id
+          senderId
+          createdAt
+          updatedAt
+          sentAt
+          seenAt
+          receivedAt
+          ackedAt
+          direction
+          senderApiVersion
+          receiverApiVersion
+          receiverId
+          kind
+          attributes
+          conversationId
+        }
+      }
+    }
+  }
+`
+
+let _subscriber = null
+
+export default context => {
+  if (_subscriber === null) {
+    _subscriber = subscriber({
+      environment: context.environment,
+      subscription: CommitLogStream,
+    })
+    _subscriber.subscribe({
+      updater: (store, data) => {
+        console.log('commit log', data)
+      },
+    })
+  }
+  return {
+    subscribe: ({ updater }) =>
+      _subscriber.subscribe({
+        updater: (store, data) => {
+          const entity = Object.values(data.entity).find(entity => entity)
+          updater(store, entity, data.operation === 2)
+        },
+      }),
+  }
+}
