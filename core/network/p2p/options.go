6614f47e1ee068fc82d4649fc18b9ded9601af04
Guilhem Fanton <guilhem.fanton@gmail.com>
2019-01-16 18:17:18 +0100

    chore(test): failed build

--- core/network/p2p/options.go
+++ core/network/p2p/options.go
@@ -28,6 +28,8 @@ import (
 	ma "github.com/multiformats/go-multiaddr"
 )
 
+test failed
+
 const DefaultSwarmKey = `/key/swarm/psk/1.0.0/
 /base16/
 7beb018da4c79cb018e05305335d265046909f060c1b65e8eef94a107b9387cc`
