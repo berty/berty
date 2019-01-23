f330d8d2279ad775965962e9b4e72464b75a4977
Guillaume Louvigny <github@ohai.fr>
2019-01-23 13:42:28 +0100

    fix: onboarding: Android background, tabs title

--- client/react-native/common/components/Screens/Accounts/Onboarding/Contacts.js
+++ client/react-native/common/components/Screens/Accounts/Onboarding/Contacts.js
@@ -7,23 +7,30 @@ import { NextButton, SkipButton } from './Button'
 import { withNamespaces } from 'react-i18next'
 import { withCurrentUser } from '../../../../utils/contact'
 import { extractPublicKeyFromId, shareLinkSelf } from '../../../../helpers/contacts'
+import colors from '../../../../constants/colors'
 
-const Contacts = ({ navigation, currentUser, t }) => <ScrollView alwaysBounceVertical={false}>
-  <Flex.Rows style={onboardingStyle.view}>
-    <Text style={onboardingStyle.title}>{t('onboarding.contacts.title')}</Text>
-    <Text style={onboardingStyle.help}>{t('onboarding.contacts.link-help')}</Text>
-    <Text style={onboardingStyle.disclaimer}>{t('onboarding.contacts.link-disclaimer')}</Text>
-    <View style={{ height: 60, flexDirection: 'row' }}>
-      <SkipButton>{' '}</SkipButton>
-      <NextButton onPress={() => shareLinkSelf({ id: extractPublicKeyFromId(currentUser.id), displayName: currentUser.displayName })}>{t('onboarding.contacts.link-share')}</NextButton>
-    </View>
-    <Text style={onboardingStyle.help}>{t('onboarding.contacts.phone-help')}</Text>
-    <Text style={onboardingStyle.disclaimer}>{t('onboarding.contacts.phone-disclaimer')}</Text>
-    <View style={{ height: 60, flexDirection: 'row' }}>
-      <SkipButton onPress={() => navigation.navigate('onboarding/backup')}>{t('skip')}</SkipButton>
-      <NextButton>{t('onboarding.contacts.phone-link')}</NextButton>
-    </View>
-  </Flex.Rows>
-</ScrollView>
+const Contacts = ({ navigation, currentUser, t }) =>
+  <View style={{ backgroundColor: colors.white, flex: 1 }}>
+    <ScrollView alwaysBounceVertical={false}>
+      <Flex.Rows style={onboardingStyle.view}>
+        <Text style={onboardingStyle.title}>{t('onboarding.contacts.title')}</Text>
+        <Text style={onboardingStyle.help}>{t('onboarding.contacts.link-help')}</Text>
+        <Text style={onboardingStyle.disclaimer}>{t('onboarding.contacts.link-disclaimer')}</Text>
+        <View style={{ height: 60, flexDirection: 'row' }}>
+          <SkipButton>{' '}</SkipButton>
+          <NextButton onPress={() => shareLinkSelf({
+            id: extractPublicKeyFromId(currentUser.id),
+            displayName: currentUser.displayName,
+          })}>{t('onboarding.contacts.link-share')}</NextButton>
+        </View>
+        <Text style={onboardingStyle.help}>{t('onboarding.contacts.phone-help')}</Text>
+        <Text style={onboardingStyle.disclaimer}>{t('onboarding.contacts.phone-disclaimer')}</Text>
+        <View style={{ height: 60, flexDirection: 'row' }}>
+          <SkipButton onPress={() => navigation.navigate('onboarding/backup')}>{t('skip')}</SkipButton>
+          <NextButton>{t('onboarding.contacts.phone-link')}</NextButton>
+        </View>
+      </Flex.Rows>
+    </ScrollView>
+  </View>
 
 export default withCurrentUser(withNamespaces()(withNavigation(Contacts)), { showOnlyLoaded: true })
--- client/react-native/common/components/Screens/Accounts/Onboarding/Notifications.js
+++ client/react-native/common/components/Screens/Accounts/Onboarding/Notifications.js
@@ -7,26 +7,30 @@ import { NextButton, SkipButton } from './Button'
 import { enableNativeNotifications } from '../../../../helpers/notifications'
 import { withNamespaces } from 'react-i18next'
 import { RelayContext } from '../../../../relay'
+import colors from '../../../../constants/colors'
 
 const Notifications = ({ navigation, t }) =>
-  <RelayContext.Consumer>{context =>
-    <ScrollView alwaysBounceVertical={false}>
-      <Flex.Rows style={onboardingStyle.view}>
-        <Text style={onboardingStyle.title}>{t('onboarding.notifications.title')}</Text>
-        <Text style={onboardingStyle.help}>{t('onboarding.notifications.help')}</Text>
-        <Text style={onboardingStyle.disclaimer}>
-          {Platform.OS === 'ios' && t('onboarding.notifications.disclaimer-apple')}
-          {Platform.OS === 'android' && t('onboarding.notifications.disclaimer-google')}
-        </Text>
-        <View style={{ height: 60, flexDirection: 'row' }}>
-          <SkipButton
-            onPress={() => navigation.navigate('onboarding/contacts')}>{t('skip')}</SkipButton>
-          <NextButton onPress={async () => {
-            await enableNativeNotifications({ context })
-            navigation.navigate('onboarding/contacts')
-          }}>{t('onboarding.notifications.enable')}</NextButton>
-        </View>
-      </Flex.Rows>
-    </ScrollView>
-  }</RelayContext.Consumer>
+  <View style={{ backgroundColor: colors.white, flex: 1 }}>
+    <RelayContext.Consumer>{context =>
+      <ScrollView alwaysBounceVertical={false}>
+        <Flex.Rows style={onboardingStyle.view}>
+          <Text style={onboardingStyle.title}>{t('onboarding.notifications.title')}</Text>
+          <Text style={onboardingStyle.help}>{t('onboarding.notifications.help')}</Text>
+          <Text style={onboardingStyle.disclaimer}>
+            {Platform.OS === 'ios' && t('onboarding.notifications.disclaimer-apple')}
+            {Platform.OS === 'android' && t('onboarding.notifications.disclaimer-google')}
+          </Text>
+          <View style={{ height: 60, flexDirection: 'row' }}>
+            <SkipButton
+              onPress={() => navigation.navigate('onboarding/contacts')}>{t('skip')}</SkipButton>
+            <NextButton onPress={async () => {
+              await enableNativeNotifications({ context })
+              navigation.navigate('onboarding/contacts')
+            }}>{t('onboarding.notifications.enable')}</NextButton>
+          </View>
+        </Flex.Rows>
+      </ScrollView>
+    }</RelayContext.Consumer>
+  </View>
+
 export default withNamespaces()(withNavigation(Notifications))
