146d3c7f52932cd86862cdb6cc94bb8f7062fe9f
Guilhem Fanton <guilhem.fanton@gmail.com>
2019-01-29 16:45:55 +0100

    fix(ios): dispatch go call to avoid UI freeze

--- client/react-native/ios/modules/core/CoreModule.swift
+++ client/react-native/ios/modules/core/CoreModule.swift
@@ -17,11 +17,16 @@ var logger = Logger("chat.berty.io", "CoreModule")
 
 @objc(CoreModule)
 class CoreModule: NSObject {
-  @objc var notificationDriver: CoreMobileNotification! = Core.notificationDriver()
+  @objc var notificationDriver: CoreMobileNotification!
+  let serialCoreQueue = DispatchQueue(label: "BertyCore")
 
   override init() {
     super.init()
-    Core.notificationDriver()?.native = Notification()
+
+    serialCoreQueue.sync {
+      self.notificationDriver = Core.notificationDriver()
+      Core.notificationDriver()?.native = Notification()
+    }
   }
 
   func getFilesDir() throws -> String {
@@ -37,182 +42,211 @@ class CoreModule: NSObject {
   }
 
   @objc func initialize(_ resolve: RCTPromiseResolveBlock!, reject: RCTPromiseRejectBlock!) {
-    var err: NSError?
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    do {
-      CoreInitialize(logger, try self.getFilesDir(), &err)
-      if let error = err {
-        throw error
+      do {
+        CoreInitialize(logger, try self.getFilesDir(), &err)
+        if let error = err {
+          throw error
+        }
+        resolve(nil)
+      } catch let error as NSError {
+        logger.format("unable to init core: %@", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.userInfo.description, error)
       }
-      resolve(nil)
-    } catch let error as NSError {
-      logger.format("unable to init core: %@", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.userInfo.description, error)
     }
   }
 
   @objc func listAccounts(_ resolve: RCTPromiseResolveBlock!, reject: RCTPromiseRejectBlock!) {
-    var err: NSError?
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    do {
-      let list = CoreListAccounts(try self.getFilesDir(), &err)
-      if let error = err {
-        throw error
+      do {
+        let list = CoreListAccounts(try self.getFilesDir(), &err)
+        if let error = err {
+          throw error
+        }
+        resolve(list)
+      } catch let error as NSError {
+        logger.format("unable to list accounts: %@", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.userInfo.description, error)
       }
-      resolve(list)
-    } catch let error as NSError {
-      logger.format("unable to list accounts: %@", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.userInfo.description, error)
     }
   }
 
   @objc func start(_ nickname: NSString, resolve: RCTPromiseResolveBlock!, reject: RCTPromiseRejectBlock!) {
-    var err: NSError?
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    do {
-      let datastore = try self.getFilesDir()
+      do {
+        let datastore = try self.getFilesDir()
 
-      guard let coreOptions = CoreMobileOptions()?
-        .withDatastorePath(datastore)?
-        .withLoggerDriver(logger)?
-        .withNickname(nickname as String)
+        guard let coreOptions = CoreMobileOptions()?
+                .withDatastorePath(datastore)?
+                .withLoggerDriver(logger)?
+                .withNickname(nickname as String)
         else {
           throw CoreError.invalidOptions
-      }
+        }
 
-      CoreStart(coreOptions, &err)
-      if let error = err {
-        throw error
+        CoreStart(coreOptions, &err)
+        if let error = err {
+          throw error
+        }
+        resolve(nil)
+      } catch let error as NSError {
+        logger.format("unable to start core: %@", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.localizedDescription, error)
       }
-      resolve(nil)
-    } catch let error as NSError {
-      logger.format("unable to start core: %@", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.localizedDescription, error)
     }
   }
 
   @objc func restart(_ resolve: RCTPromiseResolveBlock!, reject: RCTPromiseRejectBlock!) {
-    var err: NSError?
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    do {
-      CoreRestart(&err)
-      if let error = err {
-        throw error
+      do {
+        CoreRestart(&err)
+        if let error = err {
+          throw error
+        }
+        resolve(nil)
+      } catch let error as NSError {
+        logger.format("unable to restart core: %@", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.localizedDescription, error)
       }
-      resolve(nil)
-    } catch let error as NSError {
-      logger.format("unable to restart core: %@", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.localizedDescription, error)
     }
   }
 
   @objc func panic(_ resolve: RCTPromiseResolveBlock!, reject: RCTPromiseRejectBlock!) {
-    CorePanic()
-    resolve(nil)
+    serialCoreQueue.sync {
+      CorePanic()
+      resolve(nil)
+    }
   }
 
   @objc func dropDatabase(_ resolve: RCTPromiseResolveBlock!, reject: RCTPromiseRejectBlock!) {
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    var err: NSError?
-
-    do {
-      CoreDropDatabase(try self.getFilesDir(), &err)
-      if let error = err {
-        throw error
+      do {
+        CoreDropDatabase(try self.getFilesDir(), &err)
+        if let error = err {
+          throw error
+        }
+        resolve(nil)
+      } catch let error as NSError {
+        logger.format("unable to drop database: %@", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.localizedDescription, error)
       }
-      resolve(nil)
-    } catch let error as NSError {
-      logger.format("unable to drop database: %@", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.localizedDescription, error)
     }
   }
 
   @objc func getPort(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    var err: NSError?
-    var port: Int = 0
+    serialCoreQueue.sync {
+      var err: NSError?
+      var port: Int = 0
 
-    CoreGetPort(&port, &err)
-    if let error = err {
-      logger.format("unable to get port: ", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.localizedDescription, error)
-    } else {
-      resolve(port)
+      CoreGetPort(&port, &err)
+      if let error = err {
+        logger.format("unable to get port: ", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.localizedDescription, error)
+      } else {
+        resolve(port)
+      }
     }
   }
 
   @objc func getNetworkConfig(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    resolve(CoreGetNetworkConfig())
+    serialCoreQueue.sync {
+      resolve(CoreGetNetworkConfig())
+    }
   }
 
   @objc func updateNetworkConfig(_ config: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    var err: NSError?
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    CoreUpdateNetworkConfig(config, &err)
-    if let error = err {
-      logger.format("update network config error: ", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.localizedDescription, error)
-    } else {
-      resolve(nil)
+      CoreUpdateNetworkConfig(config, &err)
+      if let error = err {
+        logger.format("update network config error: ", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.localizedDescription, error)
+      } else {
+        resolve(nil)
+      }
     }
   }
 
   @objc func isBotRunning(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    resolve(CoreIsBotRunning())
+    serialCoreQueue.sync {
+      resolve(CoreIsBotRunning())
+    }
   }
 
   @objc func startBot(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    var err: NSError?
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    CoreStartBot(&err)
-    if let error = err {
-      logger.format("start bot error: ", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.localizedDescription, error)
-    } else {
-      resolve(nil)
+      CoreStartBot(&err)
+      if let error = err {
+        logger.format("start bot error: ", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.localizedDescription, error)
+      } else {
+        resolve(nil)
+      }
     }
   }
 
   @objc func stopBot(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    var err: NSError?
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    CoreStopBot(&err)
-    if let error = err {
-      logger.format("stop bot error: ", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.localizedDescription, error)
-    } else {
-      resolve(nil)
+      CoreStopBot(&err)
+      if let error = err {
+        logger.format("stop bot error: ", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.localizedDescription, error)
+      } else {
+        resolve(nil)
+      }
     }
   }
 
   @objc func getLocalGRPCInfos(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    resolve(CoreGetLocalGRPCInfos())
+    serialCoreQueue.sync {
+      resolve(CoreGetLocalGRPCInfos())
+    }
   }
 
   @objc func startLocalGRPC(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    var err: NSError?
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    CoreStartLocalGRPC(&err)
-    if let error = err {
-      logger.format("start local gRPC error: ", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.localizedDescription, error)
-    } else {
-      resolve(nil)
+      CoreStartLocalGRPC(&err)
+      if let error = err {
+        logger.format("start local gRPC error: ", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.localizedDescription, error)
+      } else {
+        resolve(nil)
+      }
     }
   }
 
   @objc func stopLocalGRPC(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-    var err: NSError?
+    serialCoreQueue.sync {
+      var err: NSError?
 
-    CoreStopLocalGRPC(&err)
-    if let error = err {
-      logger.format("stop local gRPC error: ", level: .error, error.userInfo.description)
-      reject("\(String(describing: error.code))", error.localizedDescription, error)
-    } else {
-      resolve(nil)
+      CoreStopLocalGRPC(&err)
+      if let error = err {
+        logger.format("stop local gRPC error: ", level: .error, error.userInfo.description)
+        reject("\(String(describing: error.code))", error.localizedDescription, error)
+      } else {
+        resolve(nil)
+      }
     }
   }
 
   @objc static func requiresMainQueueSetup() -> Bool {
-      return false
+    return false
   }
 }
