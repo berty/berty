adb25a5b1d2a53231f133d4b59f2c04c1607e1ea
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2019-02-11 10:50:25 +0100

    chore(deviceinfo): permit to subscribe to app state
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- core/pkg/deviceinfo/application.go
+++ core/pkg/deviceinfo/application.go
@@ -1,6 +1,12 @@
 package deviceinfo
 
-var appState = AppState_Kill
+import "sync"
+
+var (
+	appState  = AppState_Kill
+	subs      []chan AppState
+	subsMutex sync.Mutex
+)
 
 func GetAppState() AppState {
 	return appState
@@ -9,4 +15,29 @@ func GetAppState() AppState {
 func SetAppState(state AppState) {
 	logger().Info("application switch from  " + AppState_name[int32(appState)] + " to " + AppState_name[int32(state)] + " mode")
 	appState = state
+	subsMutex.Lock()
+	for i := range subs {
+		subs[i] <- appState
+	}
+	subsMutex.Unlock()
+}
+
+func SubscribeAppState() <-chan AppState {
+	sub := make(chan AppState, 1)
+	subsMutex.Lock()
+	subs = append(subs, sub)
+	subsMutex.Unlock()
+	return sub
+}
+
+func UnsubscribeAppState(sub <-chan AppState) {
+	subsMutex.Lock()
+	for i := range subs {
+		if subs[i] == sub {
+			subs = append(subs[:i], subs[i+1:]...)
+			close(subs[i])
+			break
+		}
+	}
+	subsMutex.Unlock()
 }
