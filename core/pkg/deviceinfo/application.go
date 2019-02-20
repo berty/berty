08c960051b9bcbd4edbec3df2d34432588b6c6c0
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2019-02-20 12:43:14 +0100

    chore(deviceinfo): permit to set route to app
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- core/pkg/deviceinfo/application.go
+++ core/pkg/deviceinfo/application.go
@@ -2,42 +2,46 @@ package deviceinfo
 
 import "sync"
 
-var (
-	appState  = AppState_Kill
-	subs      []chan AppState
-	subsMutex sync.Mutex
-)
+var application_state_subs []chan Application_State
+var application_state_mutex sync.Mutex
 
-func GetAppState() AppState {
-	return appState
-}
-
-func SetAppState(state AppState) {
-	logger().Info("application switch from  " + AppState_name[int32(appState)] + " to " + AppState_name[int32(state)] + " mode")
-	appState = state
-	subsMutex.Lock()
-	for i := range subs {
-		subs[i] <- appState
-	}
-	subsMutex.Unlock()
-}
-
-func SubscribeAppState() <-chan AppState {
-	sub := make(chan AppState, 1)
-	subsMutex.Lock()
-	subs = append(subs, sub)
-	subsMutex.Unlock()
+func (*Application_State) Subscribe() <-chan Application_State {
+	sub := make(chan Application_State)
+	application_state_mutex.Lock()
+	application_state_subs = append(application_state_subs, sub)
+	application_state_mutex.Unlock()
 	return sub
 }
 
-func UnsubscribeAppState(sub <-chan AppState) {
-	subsMutex.Lock()
-	for i := range subs {
-		if subs[i] == sub {
-			subs = append(subs[:i], subs[i+1:]...)
-			close(subs[i])
+func (*Application_State) Unsubscribe(sub <-chan Application_State) {
+	application_state_mutex.Lock()
+	for i := range application_state_subs {
+		if application_state_subs[i] == sub {
+			application_state_subs = append(application_state_subs[:i], application_state_subs[i+1:]...)
+			close(application_state_subs[i])
 			break
 		}
 	}
-	subsMutex.Unlock()
+	application_state_mutex.Unlock()
+}
+
+func (app *Application) SetState(state Application_State) {
+	logger().Info(
+		"application switch from  " +
+			Application_State_name[int32(app.State)] +
+			" to " +
+			Application_State_name[int32(state)] + " mode",
+	)
+
+	app.State = state
+	application_state_mutex.Lock()
+	for i := range application_state_subs {
+		application_state_subs[i] <- app.State
+	}
+	application_state_mutex.Unlock()
+}
+
+func (app *Application) SetRoute(route string) {
+	logger().Debug("application switch route from " + app.Route + " to " + route)
+	app.Route = route
 }
