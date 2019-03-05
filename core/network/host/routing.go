f660cdc9b880553e7dad58bb06089076b74fc50a
Guilhem Fanton <guilhem.fanton@gmail.com>
2019-03-05 17:15:18 +0100

    fix(p2p): Update bootstrap nodes

--- core/network/host/routing.go
+++ core/network/host/routing.go
@@ -9,11 +9,14 @@ import (
 	cid "github.com/ipfs/go-cid"
 	datastore "github.com/ipfs/go-datastore"
 	syncdatastore "github.com/ipfs/go-datastore/sync"
+	log "github.com/ipfs/go-log"
 	host "github.com/libp2p/go-libp2p-host"
 	kaddht "github.com/libp2p/go-libp2p-kad-dht"
 	inet "github.com/libp2p/go-libp2p-net"
+	peer "github.com/libp2p/go-libp2p-peer"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
 	routing "github.com/libp2p/go-libp2p-routing"
+	ropts "github.com/libp2p/go-libp2p-routing/options"
 	ma "github.com/multiformats/go-multiaddr"
 	"go.uber.org/zap"
 )
@@ -21,7 +24,6 @@ import (
 var _ routing.IpfsRouting = (*BertyRouting)(nil)
 
 type BertyRouting struct {
-	routing.IpfsRouting
 	dht     *kaddht.IpfsDHT
 	muReady sync.RWMutex
 	cready  chan struct{}
@@ -42,25 +44,70 @@ func NewBertyRouting(ctx context.Context, h host.Host, dhtSvc bool) (*BertyRouti
 		dht = kaddht.NewDHTClient(ctx, h, ds)
 	}
 
-	// Boostrap only return an error if a bad config is provided
-	// since Bootstrap use default config, it should never happened
-	if err := dht.Bootstrap(ctx); err != nil {
-		if err := dht.Close(); err != nil {
-			logger().Warn("dht bootstrap error", zap.Error(err))
-		}
-
-		return nil, err
+	br := &BertyRouting{
+		dht:    dht,
+		cready: make(chan struct{}, 1),
+		tstart: time.Now(),
 	}
 
-	br := &BertyRouting{
-		IpfsRouting: dht,
-		dht:         dht,
-		cready:      make(chan struct{}, 1),
-		tstart:      time.Now(),
+	// Bootstrap DHT
+	// Boostrap only return an error if a bad config is provided
+	// since Bootstrap use default config, it should never happened
+	if err := br.dht.Bootstrap(ctx); err != nil {
+		logger().Warn("dht bootstrap error", zap.Error(err))
 	}
 
 	h.Network().Notify(br)
 	return br, nil
+}
+
+func (br *BertyRouting) Bootstrap(ctx context.Context) error {
+	return br.dht.Bootstrap(ctx)
+}
+
+// PutValue adds value corresponding to given Key.
+func (br *BertyRouting) PutValue(ctx context.Context, k string, data []byte, opts ...ropts.Option) error {
+	if err := br.isReady(ctx); err != nil {
+		return err
+	}
+
+	return br.dht.PutValue(ctx, k, data, opts...)
+}
+
+// GetValue searches for the value corresponding to given Key.
+func (br *BertyRouting) GetValue(ctx context.Context, ns string, opts ...ropts.Option) ([]byte, error) {
+	if err := br.isReady(ctx); err != nil {
+		return nil, err
+	}
+
+	return br.dht.GetValue(ctx, ns, opts...)
+}
+
+// SearchValue searches for better and better values from this value
+// store corresponding to the given Key. By default implementations must
+// stop the search after a good value is found. A 'good' value is a value
+// that would be returned from GetValue.
+//
+// Useful when you want a result *now* but still want to hear about
+// better/newer results.
+//
+// Implementations of this methods won't return ErrNotFound. When a value
+// couldn't be found, the channel will get closed without passing any results
+func (br *BertyRouting) SearchValue(ctx context.Context, ns string, opts ...ropts.Option) (<-chan []byte, error) {
+	if err := br.isReady(ctx); err != nil {
+		logger().Error("routing isn't ready", zap.Error(err))
+	}
+
+	return br.dht.SearchValue(ctx, ns, opts...)
+}
+
+func (br *BertyRouting) FindPeer(ctx context.Context, pid peer.ID) (pstore.PeerInfo, error) {
+	if err := br.isReady(ctx); err != nil {
+		return pstore.PeerInfo{}, nil
+	}
+
+	return br.dht.FindPeer(ctx, pid)
+
 }
 
 // Provide adds the given cid to the content routing system. If 'true' is
@@ -69,9 +116,10 @@ func NewBertyRouting(ctx context.Context, h host.Host, dhtSvc bool) (*BertyRouti
 func (br *BertyRouting) Provide(ctx context.Context, id cid.Cid, brd bool) error {
 	if err := br.isReady(ctx); err != nil {
 		logger().Error("routing isn't ready", zap.Error(err))
+		return nil
 	}
 
-	return br.IpfsRouting.Provide(ctx, id, brd)
+	return br.dht.Provide(ctx, id, brd)
 }
 
 // Search for peers who are able to provide a given key
@@ -80,7 +128,7 @@ func (br *BertyRouting) FindProvidersAsync(ctx context.Context, id cid.Cid, n in
 		logger().Error("routing isn't ready", zap.Error(err))
 	}
 
-	return br.IpfsRouting.FindProvidersAsync(ctx, id, n)
+	return br.dht.FindProvidersAsync(ctx, id, n)
 }
 
 func (br *BertyRouting) isReady(ctx context.Context) error {
@@ -131,3 +179,7 @@ func (br *BertyRouting) Connected(net inet.Network, c inet.Conn) {
 }
 
 func (br *BertyRouting) Disconnected(s inet.Network, c inet.Conn) {}
+
+func init() {
+	log.SetDebugLogging()
+}
