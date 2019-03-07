f1af34bc7d830535f10792934465b8ae90681f69
Sacha Froment <sfroment42@gmail.com>
2019-03-07 13:52:48 +0100

    fix(mobile): remove microphone permission
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- client/react-native/common/components/Library/QRReader.js
+++ client/react-native/common/components/Library/QRReader.js
@@ -14,6 +14,7 @@ class QRReader extends PureComponent {
       onError={error => {
         console.error(error)
       }}
+      cameraProps={{ captureAudio: false }}
       ref={(scanner) => { this.scanner = scanner }}
       containerStyle={style}
       topViewStyle={{
