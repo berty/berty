bf00cadd10c3c5d2364376bd8136f26b8c317e67
Sacha <sfroment42@gmail.com>
2019-05-09 15:11:33 +0200

    chore(js): follow up of #1202
    Signed-off-by: Sacha <sfroment42@gmail.com>

--- client/react-native/app/bridge/service/daemon/service.js
+++
@@ -1,7 +0,0 @@
-import pbjs from 'protobufjs/light'
-import jsonDescriptor from './daemon.pb.json'
-
-// create daemon service
-const daemonRoot = pbjs.Root.fromJSON(jsonDescriptor)
-export const ServiceName = 'berty.daemon.Daemon'
-export const service = daemonRoot.lookup(ServiceName)
