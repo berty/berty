5c1232bdbd4d7f71fbdc542ed6fb790941aa8fe6
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2019-06-12 17:28:59 +0200

    fix(dashboard): use parcel instead of webpack
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- client/react-native/tools/dashboard/src/index.js
+++ client/react-native/tools/dashboard/src/index.js
@@ -1,28 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "mobx-react";
+import '@babel/polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'mobx-react'
+import './index.css'
 
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+import peers from './store/peers'
+import node from './store/node'
 
-import peers from "./store/peers";
-import node from "./store/node";
-
-import App from "./components/App";
+import App from './components/App'
 
 ReactDOM.render(
   <Provider
     {...{
       node,
-      locations: peers.locations
+      locations: peers.locations,
     }}
   >
     <App />
   </Provider>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById('root')
+)
