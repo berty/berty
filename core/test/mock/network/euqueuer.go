9b08657f4f70bef369de4993c93d8cd1d1f86fb3
Guilhem Fanton <guilhem.fanton@gmail.com>
2019-07-05 19:43:27 +0200

    chore(mock): move network mock into core/test/mock

---
+++ core/test/mock/network/euqueuer.go
@@ -0,0 +1,82 @@
+package network
+
+import (
+	"context"
+
+	"berty.tech/core/entity"
+	"berty.tech/core/pkg/tracing"
+	"berty.tech/network"
+)
+
+type Enqueuer struct {
+	network.Driver
+
+	contactID string
+	queue     chan *entity.Envelope
+	pingQueue chan string
+}
+
+func NewEnqueuer(ctx context.Context) *Enqueuer {
+	tracer := tracing.EnterFunc(ctx)
+	defer tracer.Finish()
+	// ctx = tracer.Context()
+
+	return &Enqueuer{
+		queue:     make(chan *entity.Envelope, 100),
+		pingQueue: make(chan string, 100),
+	}
+}
+
+func (e *Enqueuer) Queue() chan *entity.Envelope {
+	return e.queue
+}
+
+func (e *Enqueuer) Emit(ctx context.Context, envelope *entity.Envelope) error {
+	tracer := tracing.EnterFunc(ctx, envelope)
+	defer tracer.Finish()
+	// ctx = tracer.Context()
+
+	e.queue <- envelope
+	return nil
+}
+
+func (e *Enqueuer) Start(ctx context.Context) error {
+	tracer := tracing.EnterFunc(ctx)
+	defer tracer.Finish()
+	// ctx = tracer.Context()
+
+	select {} // wait forever
+}
+
+func (e *Enqueuer) OnEnvelopeHandler(_ func(context.Context, *entity.Envelope) (*entity.Void, error)) {
+	// doing nothing, enqueuer does not support receiving events
+}
+
+func (e *Enqueuer) PingOtherNode(ctx context.Context, destination string) error {
+	tracer := tracing.EnterFunc(ctx, destination)
+	defer tracer.Finish()
+	// ctx = tracer.Context()
+
+	e.pingQueue <- destination
+	return nil
+}
+
+func (e *Enqueuer) SetContactID(contactID string) {
+	e.contactID = contactID
+}
+
+func (e *Enqueuer) Join(ctx context.Context) error {
+	tracer := tracing.EnterFunc(ctx, e.contactID)
+	defer tracer.Finish()
+	// ctx = tracer.Context()
+
+	return nil
+}
+
+func (e *Enqueuer) Close(ctx context.Context) error {
+	tracer := tracing.EnterFunc(ctx)
+	defer tracer.Finish()
+	// ctx = tracer.Context()
+
+	return nil
+}
