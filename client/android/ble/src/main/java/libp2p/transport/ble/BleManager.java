4df7eab5b8168680d5f997c42f4c6051020494cd
aeddi <antoine.e.b@gmail.com>
2019-08-08 17:16:00 +0200

    chore(merge): small fixes

--- client/android/ble/src/main/java/libp2p/transport/ble/BleManager.java
+++ client/android/ble/src/main/java/libp2p/transport/ble/BleManager.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.ParcelUuid;
-import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
 import android.os.Build;
 import android.annotation.TargetApi;
 
