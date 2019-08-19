65cda43bd94164030d7e2da0c80b623838e7bb19
aeddi <antoine.e.b@gmail.com>
2019-08-19 18:01:13 +0200

    refactor: check/request BLE permission from react-native

--- client/packages/common/helpers/permissions.js
+++ client/packages/common/helpers/permissions.js
@@ -1,4 +1,5 @@
 import { PermissionsAndroid } from 'react-native'
+import I18n from '@berty/common/locale'
 
 export const requestAndroidPermission = async ({
   permission,
@@ -6,9 +7,14 @@ export const requestAndroidPermission = async ({
   message,
 }) => {
   try {
+    if (await PermissionsAndroid.check(permission)) return true
+
     const granted = await PermissionsAndroid.request(permission, {
       title,
       message,
+      buttonPositive: I18n.t('permission.buttonPositive'),
+      buttonNegative: I18n.t('permission.buttonNegative'),
+      buttonNeutral: I18n.t('permission.buttonNeutral'),
     })
     return granted === PermissionsAndroid.RESULTS.GRANTED
   } catch (err) {
@@ -16,4 +22,12 @@ export const requestAndroidPermission = async ({
   }
 
   return false
+}
+
+export const requestBLEAndroidPermission = async () => {
+  return await requestAndroidPermission({
+    permission: PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    title: I18n.t('permission.ble.title'),
+    message: I18n.t('permission.ble.message'),
+  })
 }
