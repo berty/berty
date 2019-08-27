dc95de0607320252c35465854b5c9292edc24375
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2019-08-27 16:47:35 +0200

    fix(android): upload sourcemap file
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- client/android/app/upload_sourcemap.sh
+++ client/android/app/upload_sourcemap.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+cd ../.. || exit
+chmod 700 "${JS_OUTPUT_SOURCE_MAP_FILE}"
 curl \
     -X POST "https://api.instabug.com/api/sdk/v3/symbols_files" \
-    -F "symbols_file=@./android-sourcemap.json" \
+    -F "symbols_file=@${JS_OUTPUT_SOURCE_MAP_FILE}" \
     -F "application_token=${INSTABUG_APP_TOKEN}" \
     -F "platform=react_native" \
     -F "os=android"
