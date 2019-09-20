3f10bf38345b670226ecfa18a2be4764f3475d5f
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2019-09-20 18:29:31 +0200

    chore(lint): typescript lint first config
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- client/packages/app/App.tsx
+++ client/packages/app/App.tsx
@@ -33,11 +33,11 @@ const App = () => {
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
           <Header />
-          {global.HermesInternal == null ? null : (
+          {/* global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
-          )}
+          )*/}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
