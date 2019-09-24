d553e66a5567522bc68a8e00e887645910ff8d89
Manfred Touron <m@42.am>
2019-09-24 16:03:15 +0200

    feat: initial cli flags parsing

--- go/cmd/bertychat/main.go
+++ go/cmd/bertychat/main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"berty.tech/go/internal/banner"
+	"github.com/peterbourgon/ff"
+	"github.com/peterbourgon/ff/ffcli"
+)
 
 func main() {
-	fmt.Println("Hello from bertychat!")
+	log.SetFlags(0)
+
+	var (
+		globalFlags = flag.NewFlagSet("bertychat", flag.ExitOnError)
+		globalDebug = globalFlags.Bool("debug", false, "debug mode")
+		bannerFlags = flag.NewFlagSet("banner", flag.ExitOnError)
+		bannerLight = bannerFlags.Bool("light", false, "light mode")
+	)
+
+	globalPreRun := func() error {
+		if *globalDebug {
+			log.Print("debug enabled")
+			// here: configure the logger
+		}
+		return nil
+	}
+
+	banner := &ffcli.Command{
+		Name:    "banner",
+		Usage:   "banner",
+		FlagSet: bannerFlags,
+		Exec: func(args []string) error {
+			if err := globalPreRun(); err != nil {
+				return err
+			}
+			if *bannerLight {
+				fmt.Println(banner.QOTD())
+			} else {
+				fmt.Println(banner.OfTheDay())
+			}
+			return nil
+		},
+	}
+
+	version := &ffcli.Command{
+		Name:  "version",
+		Usage: "version",
+		Exec: func(args []string) error {
+			fmt.Println("latest")
+			return nil
+		},
+	}
+
+	root := &ffcli.Command{
+		Usage:       "bertychat [global flags] <subcommand> [flags] [args...]",
+		FlagSet:     globalFlags,
+		Options:     []ff.Option{ff.WithEnvVarPrefix("BERTY")},
+		Subcommands: []*ffcli.Command{banner, version},
+		Exec: func([]string) error {
+			return flag.ErrHelp
+		},
+	}
+
+	if err := root.Run(os.Args[1:]); err != nil {
+		log.Fatalf("error: %v", err)
+	}
 }
--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,5 @@
 module berty.tech/go
 
 go 1.13
+
+require github.com/peterbourgon/ff v1.6.0
