8870053a89c870c11883776193ea467aaf22733b
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2019-10-07 12:05:39 +0200

    chore(js): switch to yarn for better dependencies hoist
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- js/packages/storybook-mobile/storybook/storyLoader.js
+++ js/packages/storybook-mobile/storybook/storyLoader.js
@@ -4,13 +4,11 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-	require('@berty-tech/berty-storybook/storybook.tsx');
-	require('@berty-tech/shared-storybook/storybook.tsx');
+
 }
 
 const stories = [
-	'@berty-tech/berty-storybook/storybook.tsx',
-	'@berty-tech/shared-storybook/storybook.tsx'
+
 ];
 
 module.exports = {
