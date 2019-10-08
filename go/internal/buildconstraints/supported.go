213ef71de17ce10d838acfd0b719a6be76864bc5
Manfred Touron <m@42.am>
2019-10-08 12:21:25 +0200

    chore: fix invalid comment

--- go/internal/buildconstraints/supported.go
+++ go/internal/buildconstraints/supported.go
@@ -2,5 +2,5 @@
 
 package buildconstraints // import "berty.tech/go/internal/buildconstraints"
 
-// this file is called for (version>=go1.12 AND version>go.13)
+// this file is called for (version>=go1.12 AND version<go.13)
 // see https://golang.org/pkg/go/build/
