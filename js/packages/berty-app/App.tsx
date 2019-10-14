8150824b62f10cf069816d33b4b41e3782fb3b4d
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2019-10-14 15:26:55 +0200

    fix(js): build & run berty-app
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- js/packages/berty-app/App.tsx
+++ js/packages/berty-app/App.tsx
@@ -25,22 +25,22 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen'
 
-import BertyCore from '@berty-tech/react-native-core'
+// import BertyCore from '@berty-tech/react-native-core'
+//
+// const startDaemon = (): void => {
+//   console.log('BertyCore:', BertyCore)
+//   if (BertyCore.invoke) {
+//     const promise = BertyCore.invoke('/berty.daemon.Daemon/Start', '')
+//     console.log('BertyCore.invoke("/berty.daemon.Daemon/Start", "")', promise)
+//     if (promise && promise.then) {
+//       promise.then((result: any) => console.log('.then():', result))
+//     }
+//   }
+// }
 
-const startDaemon = (): void => {
-  console.log('BertyCore:', BertyCore)
-  if (BertyCore.invoke) {
-    const promise = BertyCore.invoke('/berty.daemon.Daemon/Start', '')
-    console.log('BertyCore.invoke("/berty.daemon.Daemon/Start", "")', promise)
-    if (promise && promise.then) {
-      promise.then((result: any) => console.log('.then():', result))
-    }
-  }
-}
-
-const DebugStartDaemonButton = () => (
-  <Button title="Start daemon" onPress={startDaemon} />
-)
+// const DebugStartDaemonButton = () => (
+//   <Button title="Start daemon" onPress={startDaemon} />
+// )
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -85,50 +85,48 @@ const App = () => {
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
-        >
-          <Header />
-          {/*global.HermesInternal == null ? null : (
+      <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}
+      >
+                <Header />
+                {/*global.HermesInternal == null ? null : (
 						<View style={styles.engine}>
 							<Text style={styles.footer}>Engine: Hermes</Text>
 						</View>
 					)*/}
-          <View style={styles.engine}>
+        {/*<View style={styles.engine}>
             <DebugStartDaemonButton />
-          </View>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+          </View>*/}
+        <View style={styles.body}>
+                    <View style={styles.sectionContainer}>
+            <Text style={styles.sectionTitle}>Step One</Text>
+            <Text style={styles.sectionDescription}>
+              Edit <Text style={styles.highlight}>App.js</Text> to change this
+              screen and then come back to see your edits.
+                        </Text>
+                    </View>
+                    <View style={styles.sectionContainer}>
+            <Text style={styles.sectionTitle}>See Your Changes</Text>
+                        <Text style={styles.sectionDescription}>
+              <ReloadInstructions />
+            </Text>
+                    </View>
+                    <View style={styles.sectionContainer}>
+                        <Text style={styles.sectionTitle}>Debug</Text>
+            <Text style={styles.sectionDescription}>
+                            <DebugInstructions />
+            </Text>
+                    </View>
+          <View style={styles.sectionContainer}>
+                        <Text style={styles.sectionTitle}>Learn More</Text>
+                        <Text style={styles.sectionDescription}>
+              Read the docs to discover what to do next:
+                        </Text>
+                    </View>
+                    <LearnMoreLinks />
+        </View>
+            </ScrollView>
     </Fragment>
   )
 }
--- js/packages/react-native-example/android/src/main/java/tech/berty/example/BertyExamplePackage.java
+++ js/packages/react-native-example/android/src/main/java/tech/berty/example/BertyExamplePackage.java
@@ -8,7 +8,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
 public class BertyExamplePackage implements ReactPackage {
     @Override
