bad78eebd54175af08c7b3c9d71581eea414884e
Clement GIRARD <clemntgirard@gmail.com>
2019-10-16 18:12:21 +0200

    chore(js): start to config storybook-i18n

--- js/packages/berty-storybook/storybook.tsx
+++ js/packages/berty-storybook/storybook.tsx
@@ -2,12 +2,29 @@ import { ApplicationProvider } from 'react-native-ui-kitten'
 import { linkTo } from '@storybook/addon-links'
 import { mapping, light } from '@eva-design/eva'
 import React from 'react'
-import { storiesOf } from '@storybook/react-native'
+import { storiesOf, addParameters } from '@storybook/react-native'
 import { promiseResolved, fakeRequests, fakeConversations } from './faker'
 import * as Onboarding from './Onboarding'
 import * as Main from './Main'
 
+// import { withI18n } from "storybook-addon-i18n";
+// import "storybook-addon-i18n/register.js";
+// import { languages } from "@berty-tech/berty-i18n/locale/languages"
+
+// addParameters({
+//   i18n: {
+//     provider: null, // provider,
+//     providerProps: {
+//       // props
+//     },
+//     supportedLocales: ["en", "fr"],
+//     providerLocaleKey: "locale"
+//   }
+// });
+
 const stories = storiesOf('Berty', module)
+
+// stories.addDecorator(withI18n);
 
 stories.addDecorator((storyFn) => (
   <ApplicationProvider mapping={mapping} theme={light}>
