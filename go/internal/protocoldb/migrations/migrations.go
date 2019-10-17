0e5e4050ac0573fc2236412fd75fd05479f22808
Guillaume Louvigny <glouvigny@users.noreply.github.com>
2019-10-17 16:47:13 +0200

    feat: sqlite database models for protocol

--- go/internal/protocoldb/migrations/migrations.go
+++ go/internal/protocoldb/migrations/migrations.go
@@ -1,13 +1,13 @@
 package migrations
 
 import (
-	//"berty.tech/go/internal/protocoldb/migrations/v0001initial"
+	"berty.tech/go/internal/protocoldb/migrations/v0001init"
 	"gopkg.in/gormigrate.v1"
 )
 
 // GetMigrations returns a list of migrations for protocol database
 func GetMigrations() []*gormigrate.Migration {
 	return []*gormigrate.Migration{
-		//v0001initial.GetMigration(),
+		v0001init.GetMigration(),
 	}
 }
