74c7ca0ce68ca518e51d83601f1e01ebbe20a1de
Guilhem Fanton <guilhem.fanton@gmail.com>
2019-10-22 19:06:59 +0200

    feat(gomobile): bind gomobile bridge with bazel

---
+++ js/packages/berty-app/android/app/src/main/java/com/facebook/react/PackageList.java
@@ -0,0 +1,54 @@
+
+package com.facebook.react;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import tech.berty.app.BuildConfig;
+// import tech.berty.app.R;
+
+// tech.berty.chatsdk
+import tech.berty.chatsdk.BertyChatSdkPackage;
+
+public class PackageList {
+  private Application application;
+  private ReactNativeHost reactNativeHost;
+  public PackageList(ReactNativeHost reactNativeHost) {
+    this.reactNativeHost = reactNativeHost;
+  }
+
+  public PackageList(Application application) {
+    this.reactNativeHost = null;
+    this.application = application;
+  }
+
+  private ReactNativeHost getReactNativeHost() {
+    return this.reactNativeHost;
+  }
+
+  private Resources getResources() {
+    return this.getApplication().getResources();
+  }
+
+  private Application getApplication() {
+    if (this.reactNativeHost == null) return this.application;
+    return this.reactNativeHost.getApplication();
+  }
+
+  private Context getApplicationContext() {
+    return this.getApplication().getApplicationContext();
+  }
+
+  public ArrayList<ReactPackage> getPackages() {
+    return new ArrayList<>(Arrays.<ReactPackage>asList(
+      new MainReactPackage(),
+      new BertyChatSdkPackage()
+    ));
+  }
+}
