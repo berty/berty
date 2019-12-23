4d882038a897d25da1d47eb6de3354f56eb56367
Manfred Touron <m@42.am>
2019-12-23 17:10:23 +0100

    feat: semantic releases

---
+++ .releaserc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branch: 'master',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github',
+    ],
+};
