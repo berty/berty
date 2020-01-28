3ae70b76ef0b3674daefbbc264e688c8a8377cd9
Norman Meier <norman@berty.tech>
2020-01-28 16:28:37 +0100

    fix(berty-app/android): link react-native-interactable

---
+++ js/packages/berty-app/README.md
@@ -0,0 +1,3 @@
+## [react-native-debugger](https://github.com/jhen0409/react-native-debugger) supported version:
+
+[v0.10.5](https://github.com/jhen0409/react-native-debugger/releases/tag/v0.10.5)
--- js/packages/berty-app/android/app/src/main/java/tech/berty/app/MainApplication.java
+++ js/packages/berty-app/android/app/src/main/java/tech/berty/app/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.wix.interactable.Interactable;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages thaxt cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new Interactable());
           return packages;
         }
 
