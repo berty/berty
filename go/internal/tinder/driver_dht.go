c92994f44847aae6cd4d96a4e5b17c692590da23
Guilhem Fanton <guilhem.fanton@gmail.com>
2020-02-29 14:55:36 +0100

    feat(tinder): Add dht driver

---
+++ go/internal/tinder/driver_dht.go
@@ -0,0 +1,11 @@
+package tinder
+
+import (
+	p2p_discovery "github.com/libp2p/go-libp2p-discovery"
+	p2p_dht "github.com/libp2p/go-libp2p-kad-dht"
+)
+
+func NewDHTDriver(dht *p2p_dht.IpfsDHT) Driver {
+	disc := p2p_discovery.NewRoutingDiscovery(dht)
+	return ComposeDriver(disc, disc, NoopUnregister)
+}
