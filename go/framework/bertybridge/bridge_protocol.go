53d020f25c7d118d867869ad01eb6c373955936c
Guilhem Fanton <guilhem.fanton@gmail.com>
2020-03-19 17:01:57 +0100

    feat(protocol): Expose protocol bridge

--- go/framework/bertybridge/bridge_protocol.go
+++ go/framework/bertybridge/bridge_protocol.go
@@ -1,17 +1,31 @@
 package bertybridge
 
 import (
+	"context"
+
+	"berty.tech/berty/v2/go/internal/ipfsutil"
 	"berty.tech/berty/v2/go/pkg/bertyprotocol"
 	"berty.tech/berty/v2/go/pkg/errcode"
-	keystore "github.com/ipfs/go-ipfs-keystore"
-	ipfs_interface "github.com/ipfs/interface-go-ipfs-core"
-	"go.uber.org/zap"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.uber.org/zap"
+
+	keystore "github.com/ipfs/go-ipfs-keystore"
+	"github.com/ipfs/go-ipfs/core"
+	ipfs_interface "github.com/ipfs/interface-go-ipfs-core"
 )
 
 type Protocol struct {
 	*Bridge
 
+	node   *core.IpfsNode
 	client bertyprotocol.Client
 }
 
@@ -21,6 +35,10 @@ type ProtocolConfig struct {
 	dLogger  NativeLoggerDriver
 	loglevel string
 
+	swarmListeners   []string
+	orbitDBDirectory string
+
+	// internal
 	coreAPI ipfs_interface.CoreAPI
 }
 
@@ -30,12 +48,20 @@ func NewProtocolConfig() *ProtocolConfig {
 	}
 }
 
+func (pc *ProtocolConfig) OrbitDBDirectory(dir string) {
+	pc.orbitDBDirectory = dir
+}
+
 func (pc *ProtocolConfig) LogLevel(level string) {
 	pc.loglevel = level
 }
 
 func (pc *ProtocolConfig) LoggerDriver(dLogger NativeLoggerDriver) {
 	pc.dLogger = dLogger
+}
+
+func (pc *ProtocolConfig) AddSwarmListener(laddr string) {
+	pc.swarmListeners = append(pc.swarmListeners, laddr)
 }
 
 func NewProtocolBridge(config *ProtocolConfig) (*Protocol, error) {
@@ -59,41 +85,100 @@ func NewProtocolBridge(config *ProtocolConfig) (*Protocol, error) {
 }
 
 func newProtocolBridge(logger *zap.Logger, config *ProtocolConfig) (*Protocol, error) {
-	protocol := &Protocol{}
+	ctx := context.Background()
+
+	// setup coreapi if needed
+	var api ipfs_interface.CoreAPI
+	var node *core.IpfsNode
+	{
+		var err error
+
+		if api = config.coreAPI; api == nil {
+			swarmaddrs := []string{}
+			if len(config.swarmListeners) > 0 {
+				swarmaddrs = config.swarmListeners
+			}
+
+			api, node, err = ipfsutil.NewInMemoryCoreAPI(ctx, swarmaddrs...)
+			if err != nil {
+				return nil, errcode.TODO.Wrap(err)
+			}
+		}
+	}
 
 	// setup protocol
+	var client bertyprotocol.Client
 	{
 		var err error
 
 		// initialize new protocol client
 		protocolOpts := bertyprotocol.Opts{
 			Logger:          logger.Named("bertyprotocol"),
-			IpfsCoreAPI:     config.coreAPI,
+			IpfsCoreAPI:     api,
 			DeviceKeystore:  bertyprotocol.NewDeviceKeystore(keystore.NewMemKeystore()),
 			MessageKeystore: bertyprotocol.NewInMemMessageKeystore(),
 		}
 
-		protocol.client, err = bertyprotocol.New(protocolOpts)
+		client, err = bertyprotocol.New(protocolOpts)
 		if err != nil {
 			return nil, errcode.TODO.Wrap(err)
 		}
 	}
 
 	// register service
-	grpcServer := grpc.NewServer()
-	bertyprotocol.RegisterProtocolServiceServer(grpcServer, protocol.client)
+	var grpcServer *grpc.Server
+	{
+		grpcLogger := logger.Named("grpc.protocol")
+		// Define customfunc to handle panic
+		panicHandler := func(p interface{}) (err error) {
+			return status.Errorf(codes.Unknown, "panic recover: %v", p)
+		}
+
+		// Shared options for the logger, with a custom gRPC code to log level function.
+		recoverOpts := []grpc_recovery.Option{
+			grpc_recovery.WithRecoveryHandler(panicHandler),
+		}
+
+		zapOpts := []grpc_zap.Option{
+			grpc_zap.WithLevels(grpcCodeToLevel),
+		}
+
+		// setup grpc with zap
+		grpc_zap.ReplaceGrpcLoggerV2(grpcLogger)
+		grpcServer = grpc.NewServer(
+			grpc_middleware.WithUnaryServerChain(
+				grpc_ctxtags.UnaryServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
+
+				grpc_zap.UnaryServerInterceptor(grpcLogger, zapOpts...),
+				grpc_recovery.UnaryServerInterceptor(recoverOpts...),
+			),
+			grpc_middleware.WithStreamServerChain(
+				grpc_ctxtags.StreamServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
+				grpc_zap.StreamServerInterceptor(grpcLogger, zapOpts...),
+				grpc_recovery.StreamServerInterceptor(recoverOpts...),
+			),
+		)
+
+		bertyprotocol.RegisterProtocolServiceServer(grpcServer, client)
+	}
 
 	// setup bridge
+	var bridge *Bridge
 	{
 		var err error
 
-		protocol.Bridge, err = newBridge(grpcServer, logger, config.Config)
+		bridge, err = newBridge(grpcServer, logger, config.Config)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	return protocol, nil
+	return &Protocol{
+		Bridge: bridge,
+
+		client: client,
+		node:   node,
+	}, nil
 }
 
 func (p *Protocol) Close() (err error) {
@@ -102,6 +187,10 @@ func (p *Protocol) Close() (err error) {
 
 	// close clients and dbs after listeners
 	p.client.Close()
+
+	if p.node != nil {
+		p.node.Close()
+	}
 
 	return
 }
