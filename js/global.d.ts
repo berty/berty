f5fc6b489a1a38b9bf489a0054094ab93e39a86c
Norman Meier <norman@berty.tech>
2020-04-07 11:04:49 +0200

    feat(chat): add a way to import svg files

--- js/global.d.ts
+++ js/global.d.ts
@@ -5,3 +5,9 @@ declare interface Window {
 declare module 'simpleflakes/lib/simpleflakes-legacy'
 declare module '@clarketm/saga-monitor'
 declare module 'redux-test-recorder'
+
+declare module '*.svg' {
+	import { SvgProps } from 'react-native-svg'
+	const content: React.FC<SvgProps>
+	export default content
+}
