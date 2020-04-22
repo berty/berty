9b3f8eaac35822a61ff9f580bfc83d4bdfa7102a
Norman Meier <norman@berty.tech>
2020-04-22 05:56:36 +0200

    fix(chat): disable warning and show error string in prod

--- js/packages/berty-app/index.js
+++ js/packages/berty-app/index.js
@@ -6,14 +6,14 @@ import 'react-native-gesture-handler'
 import 'node-libs-react-native/globals'
 
 import { Alert, AppRegistry } from 'react-native'
-if (!__DEV__) {
-	/* eslint-disable no-undef */
-	console.error = () => Alert.alert('Error')
-	console.warn = () => Alert.alert('Warn')
-	/* eslint-enable no-undef */
-}
-
 import App from './App'
 import { name as appName } from './app.json'
+
+if (!__DEV__) {
+	/* eslint-disable no-undef */
+	console.error = (error) => Alert.alert('Error', `${error}`)
+	//console.warn = () => Alert.alert('Warn')
+	/* eslint-enable no-undef */
+}
 
 AppRegistry.registerComponent(appName, () => App)
