f61437b2d7469c040a433858fdd16a1f2e2af87a
Guilhem Fanton <guilhem.fanton@gmail.com>
2020-04-29 14:59:54 +0200

    chore: Update rdvp boostrap peer addr

--- go/internal/config/berty.go
+++ go/internal/config/berty.go
@@ -11,10 +11,10 @@ type BertyConfig struct {
 
 var BertyDev = &BertyConfig{
 	Bootstrap:      ipfs_cfg.DefaultBootstrapAddresses,
-	RendezVousPeer: "/dnsaddr/rdvp.berty.io/ipfs/QmTo3RS6Uc8aCS5Cxx8EBHkNCe4C7vKRanbMEboxkA92Cn",
+	RendezVousPeer: "/dnsaddr/rdvp.berty.io/ipfs/QmdT7AmhhnbuwvCpa5PH1ySK9HJVB82jr3fo1bxMxBPW6p",
 }
 
 var BertyMobile = &BertyConfig{
 	Bootstrap:      ipfs_cfg.DefaultBootstrapAddresses,
-	RendezVousPeer: "/ip4/167.99.223.55/tcp/4040/p2p/QmTo3RS6Uc8aCS5Cxx8EBHkNCe4C7vKRanbMEboxkA92Cn",
+	RendezVousPeer: "/ip4/163.172.106.31/tcp/4040/p2p/QmdT7AmhhnbuwvCpa5PH1ySK9HJVB82jr3fo1bxMxBPW6p",
 }
