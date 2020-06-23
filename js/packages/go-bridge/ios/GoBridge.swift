ccb62ecd567657db23e02ed75aaeb6d9b6dcb216
Guilhem Fanton <guilhem.fanton@gmail.com>
2020-06-23 14:46:55 +0200

    feat: add ios advanced xcbuild logs as artifact if build failed
    Signed-off-by: Guilhem Fanton <guilhem.fanton@gmail.com>

--- js/packages/go-bridge/ios/GoBridge.swift
+++ js/packages/go-bridge/ios/GoBridge.swift
@@ -73,8 +73,6 @@ class GoBridge: NSObject {
                 throw NSError(domain: "already started", code: 1)
             }
 
-            let toto: String = 2
-
             // gather opts
             let optPersistance = opts.get(bool: "persistance")
             let optLog = opts.get(string: "logLevel", defaultValue: "info")
