516bc7c9b9fd8ccbca0bce920829e540d8d4a258
Manfred Touron <94029+moul@users.noreply.github.com>
2020-06-24 21:21:19 +0200

    chore: simplify JS' Makefile(s)
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

---
+++ build/shell/release-android.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+set -x
+cd packages/berty-app
+
+npx jetify
+cd android
+echo 'android.buildCacheDir=/home/buildkite-agent/.cache/android-build-cache' >> gradle.properties
+./gradlew bundleRelease
+find . -name '*.aab'
+[ -f ~/bundletool-all-0.12.0.jar ] || wget -O ~/bundletool-all-0.12.0.jar https://github.com/google/bundletool/releases/download/0.12.0/bundletool-all-0.12.0.jar
+java -jar ~/bundletool-all-0.12.0.jar build-apks --bundle=./app/build/outputs/bundle/release/app-release.aab --output=./app.apks --mode=universal
+unzip ./app.apks
+find . -name '*.apk'
