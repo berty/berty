6a47a6b55424554677cd7190bdfe35ab82aec9ea
Elizabeth Kelen <erskelen@gmail.com>
2020-07-21 18:00:23 -0400

    fix(messenger): fix initial scaling calculation
    Signed-off-by: Elizabeth Kelen <erskelen@gmail.com>

--- js/packages/styles/constant.ts
+++ js/packages/styles/constant.ts
@@ -1,14 +1,18 @@
 import { PixelRatio, Dimensions } from 'react-native'
 
 const { height, width } = Dimensions.get('window')
-const isLandscape = height < width
 const iPhone11ShortEdge = 414
 const iPhone11LongEdge = 896
-const boundedInitialDimensions = {
-	shortEdge: Math.max(isLandscape ? height : width, iPhone11ShortEdge),
-	longEdge: Math.max(isLandscape ? width : height, iPhone11LongEdge),
+
+const dimensions = {
+	shortEdge: Math.min(height, width),
+	longEdge: Math.max(width, height),
 }
 
-export const scaleSize = Math.min(boundedInitialDimensions.shortEdge / iPhone11ShortEdge, 1)
-export const scaleHeight = Math.min(boundedInitialDimensions.longEdge / iPhone11LongEdge, 1)
+const { shortEdge, longEdge } = dimensions
+
+const shortEdgeRatio = shortEdge / Math.max(shortEdge, iPhone11ShortEdge)
+const longEdgeRatio = longEdge / Math.max(longEdge, iPhone11LongEdge)
+export const scaleSize = Math.min(shortEdgeRatio, 1)
+export const scaleHeight = Math.min(longEdgeRatio, 1)
 export const fontScale = PixelRatio.getFontScale() * scaleSize
