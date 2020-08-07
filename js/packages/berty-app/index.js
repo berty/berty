d6c10029d5327867855bc434f97cfdc4dd0fe445
Norman Meier <norman@berty.tech>
2020-08-07 15:21:45 +0200

    fix(messenger): nohoist instabug
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- js/packages/berty-app/index.js
+++ js/packages/berty-app/index.js
@@ -9,6 +9,9 @@ import { Alert, AppRegistry } from 'react-native'
 import App from './App'
 import { BridgeConsole } from '@berty-tech/go-bridge'
 import { name as appName } from './app.json'
+import Instabug from 'instabug-reactnative'
+
+Instabug.startWithToken('03c20b48603c753f8a152f589e71fbf8', [Instabug.invocationEvent.shake])
 
 if (!__DEV__) {
 	/* eslint-disable no-undef */
