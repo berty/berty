ab1f828800e96fa6ffa3a8228402e55e09a6614b
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2020-08-14 21:09:44 +1000

    chore: add unit tests for cryptoutil
    Signed-off-by: Gobin Sougrakpam <gobinsougrakpam@gmail.com>
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

---
+++ go/internal/cryptoutil/cryptoutil_test.go
@@ -0,0 +1,49 @@
+package cryptoutil
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"berty.tech/berty/v2/go/pkg/errcode"
+	"github.com/libp2p/go-libp2p-core/crypto"
+)
+
+func TestSeedFromEd25519PrivateKey(t *testing.T) {
+	priv, _, _ := crypto.GenerateECDSAKeyPair(rand.Reader)
+	_, err := SeedFromEd25519PrivateKey(priv)
+	if err != errcode.ErrInvalidInput {
+		t.Error("Should fail with ErrInvalidInput")
+	}
+	priv, _, _ = crypto.GenerateEd25519Key(rand.Reader)
+	_, err = SeedFromEd25519PrivateKey(priv)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func TestEdwardsToMontgomeryPub(t *testing.T) {
+	_, pub, _ := crypto.GenerateEd25519Key(rand.Reader)
+	_, err := EdwardsToMontgomeryPub(pub)
+	if err != nil {
+		t.Error(err)
+	}
+	_, pub, _ = crypto.GenerateECDSAKeyPair(rand.Reader)
+	_, err = EdwardsToMontgomeryPub(pub)
+	if err != errcode.ErrInvalidInput {
+		t.Error("Should fail with ErrInvalidInput")
+	}
+}
+
+func TestEdwardsToMontgomeryPriv(t *testing.T) {
+	priv, _, _ := crypto.GenerateEd25519Key(rand.Reader)
+	_, err := EdwardsToMontgomeryPriv(priv)
+	if err != nil {
+		t.Error(err)
+	}
+	priv, _, _ = crypto.GenerateECDSAKeyPair(rand.Reader)
+	_, err = EdwardsToMontgomeryPriv(priv)
+	if err != errcode.ErrInvalidInput {
+		t.Error("Should fail with ErrInvalidInput")
+	}
+}
