1646d3b1394f415b229e0618f09af566f0ae7361
Manfred Touron <94029+moul@users.noreply.github.com>
2020-09-01 01:19:26 +0200

    chore: support passing 1,true,t and various cases to configure testutil options
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- go/internal/testutil/skip.go
+++ go/internal/testutil/skip.go
@@ -1,20 +1,19 @@
 package testutil
 
 import (
-	"os"
 	"testing"
 )
 
 func SkipSlow(t *testing.T) {
 	t.Helper()
-	if os.Getenv("SKIP_SLOW_TESTS") == "1" {
+	if parseBoolFromEnv("SKIP_SLOW_TESTS") {
 		t.Skip("slow test skipped")
 	}
 }
 
 func SkipUnstable(t *testing.T) {
 	t.Helper()
-	if os.Getenv("DONT_SKIP_UNSTABLE") != "1" {
+	if !parseBoolFromEnv("DONT_SKIP_UNSTABLE") {
 		t.Log("FIXME: stabilize test")
 		t.Skip("unstable test skipped")
 	}
@@ -22,7 +21,7 @@ func SkipUnstable(t *testing.T) {
 
 func SkipBroken(t *testing.T) {
 	t.Helper()
-	if os.Getenv("DONT_SKIP_BROKEN") != "1" {
+	if !parseBoolFromEnv("DONT_SKIP_BROKEN") {
 		t.Log("FIXME: stabilize test")
 		t.Skip("broken test skipped")
 	}
