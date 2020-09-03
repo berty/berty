ee8f0f6288285b241e4b320572ce7756d4341972
Jorropo <jorropo.pgm@gmail.com>
2020-09-03 13:18:21 +0200

    Adding more format check.
    Signed-off-by: Jorropo <jorropo.pgm@gmail.com>

--- go/pkg/banner/quote_test.go
+++ go/pkg/banner/quote_test.go
@@ -4,15 +4,39 @@ import (
 	"testing"
 )
 
-func TestNoDoubleQuote(t *testing.T) {
+func TestQuotes(t *testing.T) {
 	// Only store text no author.
 	seenQuotes := []string{}
-	for _, v := range quotes {
+	for i, v := range quotes {
+		if v.Text == "" {
+			t.Errorf("Quote id : %d is empty.", i)
+		}
+		{
+			var r rune
+			for i, strv := range v.Text {
+				if i == 0 && isNotIn(strv, "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") {
+					t.Errorf("Quote : %q doesn't start with right format.", v.Text)
+				}
+				r = strv
+			}
+			if isNotIn(r, ".!?") {
+				t.Errorf("Quote : %q doesn't end with right format.", v.Text)
+			}
+		}
 		for _, sv := range seenQuotes {
 			if v.Text == sv {
-				t.Errorf("Quote : \"%s\" is being seen multiple times.", sv)
+				t.Errorf("Quote : %q is being seen multiple times.", sv)
 			}
 		}
 		seenQuotes = append(seenQuotes, v.Text)
 	}
+}
+
+func isNotIn(test rune, set string) bool {
+	for _, v := range set {
+		if test == v {
+			return false
+		}
+	}
+	return true
 }
