059bac6e17089849a190552b4e28cd409e2ceb1f
Manfred Touron <94029+moul@users.noreply.github.com>
2020-09-17 00:41:53 +0200

    chore: refactor betabot code
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- go/cmd/betabot/main.go
+++ go/cmd/betabot/main.go
@@ -2,123 +2,148 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"os/user"
 	"sync"
 
 	"berty.tech/berty/v2/go/pkg/bertymessenger"
-	"berty.tech/berty/v2/go/pkg/bertyprotocol"
-	"berty.tech/berty/v2/go/pkg/bertytypes"
-	"berty.tech/berty/v2/go/pkg/errcode"
 	"github.com/gogo/protobuf/proto"
 	"google.golang.org/grpc"
 )
 
-func main() {
-	err := betabotCommand()
+var (
+	nodeAddr    = flag.String("addr", "127.0.0.1:9091", "remote 'berty daemon' address")
+	displayName = flag.String("display-name", safeDefaultDisplayName(), "bot's display name")
+)
 
+func main() {
+	err := betabot()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error: %+v\n", err)
 		os.Exit(1)
 	}
 }
 
-func betabotCommand() error {
-	cc, err := grpc.Dial("127.0.0.1:9092", grpc.WithInsecure())
-	if err != nil {
-		return errcode.TODO.Wrap(err)
-	}
-
-	messengerClient := bertymessenger.NewMessengerServiceClient(cc)
-	protocolClient := bertyprotocol.NewProtocolServiceClient(cc)
-
-	var ctx context.Context
-
+func betabot() error {
+	flag.Parse()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	config, err := protocolClient.InstanceGetConfiguration(ctx, &bertytypes.InstanceGetConfiguration_Request{})
-	if err != nil {
-		return errcode.TODO.Wrap(err)
-	}
-
-	accountGroup, err := protocolClient.GroupInfo(ctx, &bertytypes.GroupInfo_Request{
-		GroupPK: config.AccountGroupPK,
-	})
-	if err != nil {
-		return errcode.TODO.Wrap(err)
-	}
-	fmt.Println("Group Info !", pkAsString(accountGroup.Group.PublicKey))
-
-	displayName := safeDefaultDisplayName()
-	res, err := messengerClient.InstanceShareableBertyID(ctx, &bertymessenger.InstanceShareableBertyID_Request{
-		DisplayName: displayName,
-	})
-	if err != nil {
-		return errcode.TODO.Wrap(err)
-	}
-	fmt.Println("ShareableBertyId !", res.DeepLink)
-
-	// Accept contact request
-	handlerMutex := sync.Mutex{}
-	s, err := messengerClient.EventStream(ctx, &bertymessenger.EventStream_Request{})
-	if err != nil {
-		return errcode.TODO.Wrap(err)
-	}
-	go func() {
-		for {
-			gme, err := s.Recv()
-			if err != nil {
-				log.Printf("error one: %v", err)
-			}
-			handlerMutex.Lock()
-			err = handleBetaBotEvent(ctx, messengerClient, gme)
-			handlerMutex.Unlock()
-			if err != nil {
-				log.Printf("error two: %v", err)
-			}
+	// open gRPC connection to the remote 'berty daemon' instance
+	var messengerClient bertymessenger.MessengerServiceClient
+	{
+		cc, err := grpc.Dial(*nodeAddr, grpc.WithInsecure())
+		if err != nil {
+			return fmt.Errorf("cannot connect to remote berty daemon: %w", err)
 		}
-	}()
-	WaitForCtrlC()
+		messengerClient = bertymessenger.NewMessengerServiceClient(cc)
+	}
+
+	// get sharing link
+	{
+		req := &bertymessenger.InstanceShareableBertyID_Request{DisplayName: *displayName}
+		res, err := messengerClient.InstanceShareableBertyID(ctx, req)
+		if err != nil {
+			return err
+		}
+		log.Printf("berty id: %s", res.DeepLink)
+		// FIXME: display QR code
+	}
+
+	// event loop
+	{
+		s, err := messengerClient.EventStream(ctx, &bertymessenger.EventStream_Request{})
+		if err != nil {
+			return err
+		}
+
+		handlerMutex := sync.Mutex{}
+		go func() {
+			for {
+				gme, err := s.Recv()
+				if err != nil {
+					log.Printf("stream error: %v", err)
+				}
+				handlerMutex.Lock()
+				err = handleEvent(ctx, messengerClient, gme)
+				handlerMutex.Unlock()
+				if err != nil {
+					log.Printf("event handler error: %v", err)
+				}
+			}
+		}()
+	}
+
+	waitForCtrlC()
 	return nil
 }
 
-func handleBetaBotEvent(ctx context.Context, messengerClient bertymessenger.MessengerServiceClient, gme *bertymessenger.EventStream_Reply) error {
-	var err error
+func handleEvent(ctx context.Context, messengerClient bertymessenger.MessengerServiceClient, gme *bertymessenger.EventStream_Reply) error {
+	log.Printf("new event: %q", gme.Event.Type)
 
-	fmt.Println("HandleBetaBotEvent : ", gme.Event.Type)
+	// parse event's payload
+	update, err := gme.Event.UnmarshalPayload()
+	if err != nil {
+		return err
+	}
+
 	switch gme.Event.Type {
 	case bertymessenger.StreamEvent_TypeContactUpdated:
-		update, e := gme.Event.UnmarshalPayload()
-		if e != nil {
-			return e
-		}
+		// auto-accept contact requests
 		contact := update.(*bertymessenger.StreamEvent_ContactUpdated).Contact
 		if contact.State == bertymessenger.Contact_IncomingRequest {
-			pkb, e := stringToBytes(contact.PublicKey)
-			if e != nil {
-				return fmt.Errorf("pkg err: %w", err)
+			req := &bertymessenger.ContactAccept_Request{PublicKey: contact.PublicKey}
+			_, err = messengerClient.ContactAccept(ctx, req)
+			if err != nil {
+				return err
 			}
-			_, err = messengerClient.ContactAccept(ctx, &bertymessenger.ContactAccept_Request{PublicKey: bytesToString(pkb)})
 		}
+
 	case bertymessenger.StreamEvent_TypeInteractionUpdated:
-		update, e := gme.Event.UnmarshalPayload()
-		if e != nil {
-			return e
-		}
+		// auto-reply to users' messages
 		interaction := update.(*bertymessenger.StreamEvent_InteractionUpdated).Interaction
 		if interaction.Type == bertymessenger.AppMessage_TypeUserMessage && !interaction.IsMe && !interaction.Acknowledged {
-			userMessage, e := proto.Marshal(&bertymessenger.AppMessage_UserMessage{Body: "Welcome to the beta!"})
-			if e != nil {
-				return e
+			userMessage, err := proto.Marshal(&bertymessenger.AppMessage_UserMessage{
+				Body: "Welcome to the beta!",
+			})
+			if err != nil {
+				return err
 			}
+
 			_, err = messengerClient.Interact(ctx, &bertymessenger.Interact_Request{
 				Type:                  bertymessenger.AppMessage_TypeUserMessage,
 				Payload:               userMessage,
 				ConversationPublicKey: interaction.ConversationPublicKey,
 			})
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return err
+}
+
+func waitForCtrlC() {
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+	<-signalChannel
+}
+
+func safeDefaultDisplayName() string {
+	var name string
+	current, err := user.Current()
+	if err == nil {
+		name = current.Username
+	}
+	if name == "" {
+		name = os.Getenv("USER")
+	}
+	if name == "" {
+		name = "Anonymous4242"
+	}
+	return fmt.Sprintf("%s (bot)", name)
 }
