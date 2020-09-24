7524c7a5ad157a6685c4587d8837e20ba3bc6edf
Elizabeth Kelen <erskelen@gmail.com>
2020-09-24 18:17:27 -0400

    feat: enhance notification body component
    feat: replace airplane with color berd
    
    build: add x86 valid ios arch option
    
    refactor: notif component
    
    Signed-off-by: Elizabeth Kelen <erskelen@gmail.com>

--- js/packages/components/NotificationBody.tsx
+++ js/packages/components/NotificationBody.tsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useRef } from 'react'
-import {
-	TouchableOpacity,
-	StatusBar,
-	View,
-	Text,
-	Image,
-	Vibration,
-	ImageSourcePropType,
-} from 'react-native'
+import { TouchableOpacity, StatusBar, View, Text, Vibration } from 'react-native'
 import GestureRecognizer from 'react-native-swipe-gestures'
 import { SafeAreaContext } from 'react-native-safe-area-context'
-import { Icon } from 'react-native-ui-kitten'
 
 import { messenger as messengerpb } from '@berty-tech/api/index.js'
 import { useStyles } from '@berty-tech/styles'
 import { useInteraction, useConversation, useOneToOneContact } from '@berty-tech/store/hooks'
 
-import { ProceduralCircleAvatar } from './shared-components'
 import BlurView from './shared-components/BlurView'
+import Logo from './main/1_berty_picto.svg'
+
+//
+// Styles
+//
+
+const useStylesNotification = () => {
+	const [{ flex }] = useStyles()
+	return {
+		touchable: [flex.tiny, flex.direction.row, { paddingHorizontal: 10 }],
+		innerTouchable: [flex.direction.row, { padding: 15, flexGrow: 0, flexShrink: 1 }],
+		titleAndTextWrapper: [flex.justify.spaceAround, { marginLeft: 15, flexShrink: 1, flexGrow: 0 }],
+	}
+}
 
 function usePrevious(value) {
 	// https://blog.logrocket.com/how-to-get-previous-props-state-with-react-hooks/
@@ -28,17 +32,47 @@ function usePrevious(value) {
 	return ref.current
 }
 
-const NotificationMessage = ({ onClose, title, message, padding }) => {
-	const [{ text, margin, opacity }] = useStyles()
+//
+// Components
+//
+
+const NotificationTmpLogo: React.FC<{}> = () => {
+	return (
+		<View
+			style={{
+				alignSelf: 'center',
+				alignItems: 'center',
+				width: 40,
+				height: 40,
+				flexGrow: 0,
+				flexShrink: 0,
+				borderRadius: 30,
+				justifyContent: 'center',
+
+				borderWidth: 2,
+				borderColor: '#bbbee4',
+			}}
+		>
+			{/*<Icon name='checkmark-outline' fill={color.green} width={15} height={15} />*/}
+			<Logo
+				width={26}
+				height={26}
+				style={{ marginLeft: 4 }} // nudge logo to appear centered
+			/>
+		</View>
+	)
+}
+
+const NotificationMessage: React.FC<any> = ({ onClose, title, message, ...props }) => {
+	const [{ text }] = useStyles()
+	const _styles = useStylesNotification()
+	const { conversation, interaction } = props?.additionalProps?.payload?.payload || {}
 
 	return (
 		<>
 			<TouchableOpacity
-				style={{
-					flex: 1,
-					flexDirection: 'row',
-				}}
-				activeOpacity={0.3}
+				style={_styles.touchable}
+				activeOpacity={1} // TODO: Enable navigation, then re-add activeOpacity
 				//underlayColor='transparent'
 				onPress={() => {
 					if (typeof onClose === 'function') {
@@ -46,174 +80,56 @@ const NotificationMessage = ({ onClose, title, message, padding }) => {
 					}
 				}}
 			>
-				<View
-					style={{
-						// marginTop: 10,
-						marginLeft: 10,
-						resizeMode: 'contain',
-						width: 24,
-						height: 40,
-						borderRadius: 12,
-					}}
-				>
-					<ProceduralCircleAvatar
-						// TODO publickKey of the conv
-						seed='1igPxTFHkJzCYlK84Xxyn1AI1zN/T+8kGZFqmjlAS58='
-						size={40}
-					/>
-				</View>
-				<View
-					style={{
-						alignSelf: 'center',
-						marginLeft: 30,
-						justifyContent: 'space-evenly',
-					}}
-				>
-					<Text
-						numberOfLines={1}
-						style={[
-							{
-								color: 'black',
-							},
-							text.bold.small,
-							margin.right.tiny,
-						]}
-					>
-						{title}
-					</Text>
-					<Text
-						numberOfLines={1}
-						style={[
-							{
-								color: 'black',
-							},
-							opacity(0.8),
-						]}
-					>
-						{message}
-					</Text>
+				<View style={_styles.innerTouchable}>
+					<NotificationTmpLogo />
+					<View style={_styles.titleAndTextWrapper}>
+						<Text numberOfLines={1} style={[text.color.black, text.bold.medium]}>
+							{title}
+						</Text>
+						<Text numberOfLines={1} ellipsizeMode='tail' style={[text.color.black]}>
+							{message}
+						</Text>
+					</View>
 				</View>
 			</TouchableOpacity>
-			<View
-				style={{
-					alignSelf: 'center',
-					margin: 5,
-					marginTop: padding - 10,
-				}}
-			/>
 		</>
 	)
 }
 
-const NotificationBasic: React.FC<any> = ({
-	onClose,
-	padding,
-	title,
-	message,
-	additionalProps: notif,
-}) => {
-	const [{ color, text, margin, bold }] = useStyles()
+const NotificationBasic: React.FC<any> = ({ onClose, title, message, additionalProps: notif }) => {
+	const [{ text }] = useStyles()
+	const _styles = useStylesNotification()
 	console.log('notif', notif)
 	if (!notif) {
 		return null
 	}
 	return (
-		<>
-			<TouchableOpacity
-				style={{
-					flex: 1,
-					flexDirection: 'row',
-				}}
-				activeOpacity={0.3}
-				//underlayColor='transparent'
-				onPress={() => {
-					if (typeof onClose === 'function') {
-						onClose()
-					}
-				}}
-			>
-				<View
-					style={{
-						// marginTop: 10,
-						marginLeft: 20,
-						resizeMode: 'contain',
-						width: 24,
-						height: 40,
-						borderRadius: 12,
-					}}
-				>
-					<View
-						style={{
-							alignSelf: 'center',
-							alignItems: 'center',
-							width: 40,
-							height: 40,
-							backgroundColor: color.green,
-							borderRadius: 20,
-							justifyContent: 'center',
-						}}
-					>
-						<Icon name='paper-plane-outline' width={20} height={20} fill={color.white} />
-					</View>
+		<TouchableOpacity
+			style={_styles.touchable}
+			activeOpacity={0.3}
+			//underlayColor='transparent'
+			onPress={() => {
+				if (typeof onClose === 'function') {
+					onClose()
+				}
+			}}
+		>
+			<View style={_styles.innerTouchable}>
+				<NotificationTmpLogo />
+				<View style={_styles.titleAndTextWrapper}>
+					<Text numberOfLines={1} style={[text.color.black, text.bold.medium]}>
+						{title}
+					</Text>
+					<Text numberOfLines={1} ellipsizeMode='tail' style={[text.color.black]}>
+						{message}
+					</Text>
 				</View>
-				<View>
-					<View
-						style={{
-							alignSelf: 'flex-start',
-							marginLeft: 20,
-							flexDirection: 'row',
-						}}
-					>
-						<Text
-							numberOfLines={1}
-							style={[
-								{
-									color: 'black',
-								},
-								text.bold.medium,
-								margin.right.tiny,
-							]}
-						>
-							{title}
-						</Text>
-					</View>
-					<View
-						style={{
-							alignSelf: 'flex-start',
-							marginLeft: 20,
-							flexDirection: 'row',
-						}}
-					>
-						<Text
-							numberOfLines={1}
-							ellipsizeMode='tail'
-							style={[
-								{
-									color: 'black',
-									width: 100,
-								},
-
-								margin.right.tiny,
-							]}
-						>
-							{message}
-						</Text>
-						{/*<Icon name='checkmark-outline' fill={color.green} width={15} height={15} />*/}
-					</View>
-				</View>
-			</TouchableOpacity>
-			<View
-				style={{
-					alignSelf: 'center',
-					margin: 5,
-					marginTop: padding - 10,
-				}}
-			/>
-		</>
+			</View>
+		</TouchableOpacity>
 	)
 }
 
-const NotificationBody = (props) => {
+const NotificationBody: React.FC<any> = (props) => {
 	console.log('RENDERING NOTIFICATION', props)
 
 	const prevProps = usePrevious(props)
@@ -229,7 +145,13 @@ const NotificationBody = (props) => {
 		return null
 	}
 
-	const padding = 15
+	const NotificationContents = () =>
+		props?.additionalProps?.type === 2 ? (
+			<NotificationMessage {...props} />
+		) : (
+			<NotificationBasic {...props} />
+		)
+
 	return (
 		<SafeAreaContext.Consumer>
 			{(insets) => (
@@ -255,13 +177,11 @@ const NotificationBody = (props) => {
 							marginTop: insets?.top || 0,
 							width: '90%',
 							borderRadius: 15,
-							paddingTop: padding,
 						}}
 						blurType='xlight'
 						blurAmount={10}
 					>
-						<NotificationBasic {...props} padding={padding} />
-						{/* <NotificationMessage {...props} padding={padding} /> */}
+						<NotificationContents />
 					</BlurView>
 				</GestureRecognizer>
 			)}
