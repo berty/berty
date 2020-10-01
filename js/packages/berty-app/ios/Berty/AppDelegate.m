9ada8ea91f3fe939ee0c9fcb45afaf2fcbbeaaff
Guilhem Fanton <guilhem.fanton@gmail.com>
2020-10-01 15:18:39 +0200

    chore: remove react-native-config
    Signed-off-by: Guilhem Fanton <guilhem.fanton@gmail.com>

--- js/packages/berty-app/ios/Berty/AppDelegate.m
+++ js/packages/berty-app/ios/Berty/AppDelegate.m
@@ -13,7 +13,8 @@
 #import <React/RCTLinkingManager.h>
 #import "Berty-Swift.h"
 
-#ifdef FB_SONARKIT_ENABLED
+#if FB_SONARKIT_ENABLED
+
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -30,6 +31,7 @@ static void InitializeFlipper(UIApplication *application) {
   [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
   [client start];
 }
+
 #endif
 
 // TODO: remove this after closed beta / replace it by a more privacy complient solution
@@ -55,7 +57,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#ifdef FB_SONARKIT_ENABLED
+#if FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
 
