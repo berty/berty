ead6811b88b4a9b24ad1cd7b75534d9c2e6ffe08
Manfred Touron <94029+moul@users.noreply.github.com>
2020-10-13 01:13:19 +0200

    chore: update config/ structure, add the Conversation type, add more contacts, and more metadata
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- go/pkg/bertytypes/config.go
+++ go/pkg/bertytypes/config.go
@@ -4,8 +4,15 @@ package bertytypes
 type Config struct {
 	Berty struct {
 		Contacts map[string]struct {
-			Link string `json:"link"`
+			Link        string `json:"link"`
+			Name        string `json:"name"`
+			Description string `json:"description"`
 		} `json:"contacts"`
+		Conversations map[string]struct {
+			Link        string `json:"link"`
+			Name        string `json:"name"`
+			Description string `json:"description"`
+		} `json:"conversations"`
 	} `json:"berty"`
 	P2P struct {
 		RDVP []struct {
