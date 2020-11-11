5fdd527d8a8c3c8cdefc2377286ecd6cd1b25583
Omar Awamry <omar.wamry@gmail.com>
2020-11-11 15:33:08 +0200

    fix 2654
    Signed-off-by: Omar Awamry <omar.wamry@gmail.com>

--- js/packages/components/shared-components/HintBody.js
+++ js/packages/components/shared-components/HintBody.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import { View, Text as TextNative } from 'react-native'
-import { Icon } from '@ui-kitten/components'
-
+import Quote from './quote.svg'
 import { useStyles } from '@berty-tech/styles'
 
 import messengerMethodsHooks from '@berty-tech/store/methods'
 
 const useStylesHint = () => {
-	const [{ text, opacity, margin }, { fontScale }] = useStyles()
+	const [{ text, margin }, { fontScale }] = useStyles()
 	return {
 		searchHintBodyText: [
 			text.align.center,
-			text.color.black,
 			text.size.medium,
 			text.bold.small,
-			opacity(0.8),
 			margin.top.medium,
 			margin.bottom.large,
-			{ fontFamily: 'Open Sans', lineHeight: 30 * fontScale },
+			{ fontFamily: 'Open Sans', lineHeight: 30 * fontScale, color: '#DBE1EC' },
 		],
 	}
 }
@@ -25,7 +22,7 @@ const useStylesHint = () => {
 const _landingIconSize = 45
 
 export const HintBody = () => {
-	const [{ padding, opacity, row, text }, { scaleSize }] = useStyles()
+	const [{ padding, row, text }, { scaleSize }] = useStyles()
 	const { searchHintBodyText } = useStylesHint()
 
 	const { reply: bannerQuote = {}, call, called } = messengerMethodsHooks.useBannerQuote()
@@ -41,24 +38,21 @@ export const HintBody = () => {
 				style={[
 					text.align.center,
 					row.item.justify,
-					text.color.black,
 					text.size.scale(30),
-					opacity(0.8),
 					text.bold.medium,
 					{
 						fontFamily: 'Open Sans',
 						marginHorizontal: _landingIconSize * scaleSize, // room for speech bubble icon
+						color: '#DADDE6',
 					},
 				]}
 			>
 				{'Quote of the day'}
 			</TextNative>
-			<Icon
-				name='quote'
-				pack='custom'
+			<Quote
 				width={_landingIconSize * scaleSize}
 				height={_landingIconSize * scaleSize}
-				style={[row.item.justify, opacity(0.8), { position: 'absolute', bottom: 20, right: 10 }]}
+				style={{ position: 'absolute', bottom: 150, right: 30 }}
 			/>
 			<TextNative style={searchHintBodyText}>{bannerQuote?.quote || ''}</TextNative>
 			{bannerQuote?.author && (
@@ -68,8 +62,7 @@ export const HintBody = () => {
 							text.color.black,
 							text.size.medium,
 							text.bold.small,
-							opacity(0.8),
-							{ fontFamily: 'Open Sans' },
+							{ fontFamily: 'Open Sans', color: '#DBE1EC' },
 						]}
 					>
 						{'— ' + bannerQuote?.author}
