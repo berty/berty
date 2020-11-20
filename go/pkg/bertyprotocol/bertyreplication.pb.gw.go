b066e3c3c11c8ddbbfa265530aa551f72aee22b1
Manfred Touron <94029+moul@users.noreply.github.com>
2020-11-20 12:51:00 +0100

    chore: use go1.15 in bertytech/protoc:26
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- go/pkg/bertyprotocol/bertyreplication.pb.gw.go
+++ go/pkg/bertyprotocol/bertyreplication.pb.gw.go
@@ -20,6 +20,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 
 	"berty.tech/berty/v2/go/pkg/bertytypes"
@@ -33,6 +34,7 @@ var (
 	_ = runtime.String
 	_ = utilities.NewDoubleArray
 	_ = descriptor.ForMessage
+	_ = metadata.Join
 )
 
 func request_ReplicationService_ReplicateGroup_0(ctx context.Context, marshaler runtime.Marshaler, client ReplicationServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
@@ -70,10 +72,13 @@ func local_request_ReplicationService_ReplicateGroup_0(ctx context.Context, mars
 // RegisterReplicationServiceHandlerServer registers the http handlers for service ReplicationService to "mux".
 // UnaryRPC     :call ReplicationServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
+// Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterReplicationServiceHandlerFromEndpoint instead.
 func RegisterReplicationServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server ReplicationServiceServer) error {
 	mux.Handle("POST", pattern_ReplicationService_ReplicateGroup_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
 		if err != nil {
@@ -81,6 +86,7 @@ func RegisterReplicationServiceHandlerServer(ctx context.Context, mux *runtime.S
 			return
 		}
 		resp, md, err := local_request_ReplicationService_ReplicateGroup_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
