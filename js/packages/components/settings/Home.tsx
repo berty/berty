c8477b5021b478bee40b8aff72e5698fb0bf9a1a
Manfred Touron <94029+moul@users.noreply.github.com>
2020-11-24 14:38:16 +0100

    Revert "Merge pull request #2723 from sakul-budhathoki/feat/custom-qr-#2712"
    This reverts commit 95759f635e8cd2cf92d21c6f111675ce096461a9, reversing
    changes made to e6465f5e31519f4d2c93826e38d68f9ffa6ad002.
    
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- js/packages/components/settings/Home.tsx
+++ js/packages/components/settings/Home.tsx
@@ -13,7 +13,6 @@ import { MessengerActions } from '@berty-tech/store/context'
 import { ButtonSetting, ButtonSettingRow } from '../shared-components/SettingsButtons'
 import HeaderSettings from '../shared-components/Header'
 import { SwipeNavRecognizer } from '../shared-components/SwipeNavRecognizer'
-import logo from '../main/1_berty_picto.png'
 import { AccountAvatar } from '../avatars'
 
 const useStylesHome = () => {
@@ -100,10 +99,7 @@ const HomeHeaderAvatar: React.FC = () => {
 					</View>
 					<Text style={[_styles.headerNameText]}>{account?.displayName || ''}</Text>
 					<View style={[padding.top.scale(20 * scaleHeight)]}>
-						{(account?.link && (
-							<QRCode size={qrCodeSize} value={account.link} logo={logo} color='#3845E0' />
-						)) ||
-							null}
+						{(account?.link && <QRCode size={qrCodeSize} value={account.link} />) || null}
 					</View>
 				</View>
 			</TouchableOpacity>
