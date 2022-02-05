de27db958f40168a959347ee6ba7e8781accd8b9
Xavier Le Cunff <xavier.le-cunff@azot.dev>
2022-02-05 14:44:35 +0100

    feat: remove console methods calls in production
    Signed-off-by: Xavier Le Cunff <xavier.le-cunff@azot.dev>

--- js/babel.config.js
+++ js/babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
 	presets: ['module:metro-react-native-babel-preset'],
+	env: {
+		production: {
+			plugins: ['transform-remove-console'],
+		},
+	},
 	plugins: [
 		[
 			'module-resolver',
