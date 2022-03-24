09c68cedee2deb437701b5df64376962e123ac36
Manfred Touron <94029+moul@users.noreply.github.com>
2022-03-24 17:24:43 +0000

    fix: support more architectures with goreleaser
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- .releaserc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    branch: 'master',
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github',
-    ],
-};
