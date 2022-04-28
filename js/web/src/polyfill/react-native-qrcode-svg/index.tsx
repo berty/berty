81f661883d54930c58554ae23c2334a3dc46fd19
Xavier Le Cunff <xavier.le-cunff@azot.dev>
2022-04-28 14:42:43 +0200

    feat: add qr code on web
    Signed-off-by: Xavier Le Cunff <xavier.le-cunff@azot.dev>

--- js/web/src/polyfill/react-native-qrcode-svg/index.tsx
+++ js/web/src/polyfill/react-native-qrcode-svg/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { FC } from 'react'
+import ImportedQRCode, { QRCodeProps } from '../../../../node_modules/react-native-qrcode-svg' // import from original bottom-sheet
 
-const QRCode = () => <></>
+const QRCode: FC<QRCodeProps> = ({ logo, mode, ...props }) => <ImportedQRCode {...props} />
+
 export default QRCode
