4eb716975058aae00d598b7610e6d6412d9a7128
Norman Meier <norman@berty.tech>
2022-05-06 22:34:19 +0200

    chore: remove dead code in @berty/grpc-bridge
    Signed-off-by: Norman Meier <norman@berty.tech>

--- js/packages/grpc-bridge/index.ts
+++ js/packages/grpc-bridge/index.ts
@@ -1,9 +1,2 @@
-import grpcBridge from './bridge'
-
-export type { GRPCBridge } from './bridge'
-export { ReactNativeTransport } from './grpc-web-react-native-transport'
-export { WebsocketTransport } from './grpc-web-websocket-transport'
 export { createServiceClient } from './service'
 export { GRPCError, EOF } from './error'
-
-export default grpcBridge
