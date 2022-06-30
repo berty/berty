285df08dfa177db2e2b6cade314c278992efcb1b
clegirar <clemntgirard@gmail.com>
2022-06-30 10:34:43 +0200

    fix: OneToOne screen test
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- js/packages/utils/testing/helpers/messengerStore.test.ts
+++ js/packages/utils/testing/helpers/messengerStore.test.ts
@@ -13,11 +13,23 @@ export const getFirstMultiMemberConv = () => {
 
 export const getFirstOneToOneConv = () => {
 	const state = store.getState()
-	const conv = Object.values(state.messenger.conversations.entities).find(
-		conv =>
+	const conv = Object.values(state.messenger.conversations.entities).find(conv => {
+		if (
 			(conv?.type as keyof typeof beapi.messenger.Conversation.Type | unknown) ===
-			beapi.messenger.Conversation.Type[beapi.messenger.Conversation.Type.ContactType],
-	)
+			beapi.messenger.Conversation.Type[beapi.messenger.Conversation.Type.ContactType]
+		) {
+			// recup the contact of the conv to check if the request is accepted
+			const contact = Object.values(state.messenger.contacts.entities).find(
+				contact => contact?.publicKey === conv?.contactPublicKey,
+			)
+			if (
+				(contact?.state as keyof typeof beapi.messenger.Contact.State | unknown) ===
+				beapi.messenger.Contact.State[beapi.messenger.Contact.State.Accepted]
+			) {
+				return conv
+			}
+		}
+	})
 	return conv
 }
 
