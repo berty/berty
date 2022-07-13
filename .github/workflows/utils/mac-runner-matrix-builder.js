90c8f6c3c6d5c3684cc4fb29e4c1c8e39aeacf5f
aeddi <antoine.e.b@gmail.com>
2022-07-13 17:58:56 +0200

    ci: cleanup workflows and switch tool setup to asdf
    Signed-off-by: aeddi <antoine.e.b@gmail.com>

--- .github/workflows/utils/mac-runner-matrix-builder.js
+++ .github/workflows/utils/mac-runner-matrix-builder.js
@@ -1,55 +1,44 @@
 #!/usr/bin/env node
-const statusGetter = require("./get-selfhosted-runner-status");
+const statusGetter = require("./get-self-hosted-runner-status");
 
 // Check parameters
-const mode = process.argv[2] || "random";
+const mode = process.argv[2] || "self-hosted";
 const token = process.argv[3];
 
 if (
-	!["random", "all", "dc4", "github", "optimized"].includes(mode) ||
+	!["self-hosted", "github", "optimized"].includes(mode) ||
 	(mode === "optimized" && process.argv.length !== 4) ||
 	(mode !== "optimized" && process.argv.length !== 3 && process.argv[2])
 ) {
 	console.error(
-		`Usage: ${process.argv[1]} [random | all | dc4 | github | optimized <github_token>]`
+		`Usage: ${process.argv[1]} [self-hosted | github | optimized <github_token>]`
 	);
 	process.exit(1);
 }
 
 // Returns matrix according to parameters and self-hosted runner availability
 async function getMatrix() {
-	const matrixBase = {
-		golang: "1.17.x",
-		xcode: "13.x",
-		node: "16.x",
-	};
 	const runners = {
-		dc4: {
-			name: "dc4",
-			runner: ["self-hosted", "dc4"],
+		selfhosted: {
+			name: "self-hosted",
+			runner: ["self-hosted", "self-hosted"],
 			selfhosted: true,
-			...matrixBase,
 		},
 		github: {
 			name: "github",
-			runner: ["macos-11"],
+			runner: ["macos-12"],
 			selfhosted: false,
-			...matrixBase,
 		},
 	};
 
 	switch (mode) {
-		case "random":
-			return Math.random() < 0.5 ? [runners.dc4] : [runners.github];
-		case "all": // (in parallel)
-			return [runners.dc4, runners.github];
-		case "dc4":
-			return [runners.dc4];
+		case "self-hosted":
+			return [runners.selfhosted];
 		case "github":
 			return [runners.github];
 		case "optimized":
-			return (await statusGetter.isDC4RunnerAvailable(token))
-				? [runners.dc4]
+			return (await statusGetter.isSelfHostedRunnerAvailable(token))
+				? [runners.selfhosted]
 				: [runners.github];
 	}
 }
