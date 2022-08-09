ca16e5b7331b868ad822c2c52f2e307a9d7e539b
gfanton <8671905+gfanton@users.noreply.github.com>
2022-08-09 12:01:54 +0200

    chore: add pstatus logger
    Signed-off-by: gfanton <8671905+gfanton@users.noreply.github.com>

--- go/pkg/bertyprotocol/api_group.go
+++ go/pkg/bertyprotocol/api_group.go
@@ -2,6 +2,7 @@ package bertyprotocol
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/hex"
 	"fmt"
 	"strings"
@@ -105,6 +106,9 @@ func (s *service) GroupDeviceStatus(req *protocoltypes.GroupDeviceStatus_Request
 	gkey := hex.EncodeToString(req.GroupPK)
 	peers := PeersConnectedness{}
 
+	logger := s.logger.Named("pstatus")
+	logger.Debug("start monitor device status group", logutil.PrivateString("group_key", gkey))
+
 	for {
 		updated, ok := s.peerStatusManager.WaitForConnectednessChange(ctx, gkey, peers)
 		if !ok {
@@ -122,16 +126,26 @@ func (s *service) GroupDeviceStatus(req *protocoltypes.GroupDeviceStatus_Request
 				var connected *protocoltypes.GroupDeviceStatus_Reply_PeerConnected
 				if connected, err = s.craftPeerConnectedMessage(peer); err == nil {
 					evt.Event, err = connected.Marshal()
+					logger.Debug("peer connected",
+						logutil.PrivateString("group_key", gkey),
+						logutil.PrivateString("peer", connected.PeerID),
+						logutil.PrivateString("devicePK", base64.URLEncoding.EncodeToString(connected.GetDevicePK())))
 				}
 
 			case ConnectednessTypeDisconnected:
 				evt.Type = protocoltypes.TypePeerDisconnected
 				disconnected := s.craftDeviceDisconnectedMessage(peer)
+				logger.Debug("peer disconnected",
+					logutil.PrivateString("group_key", gkey),
+					logutil.PrivateString("peer", disconnected.PeerID))
 				evt.Event, err = disconnected.Marshal()
 
 			case ConnectednessTypeReconnecting:
 				evt.Type = protocoltypes.TypePeerConnected
 				reconnecting := s.craftDeviceReconnectedMessage(peer)
+				logger.Debug("peer reconnecting",
+					logutil.PrivateString("group_key", gkey),
+					logutil.PrivateString("peer", reconnecting.PeerID))
 				evt.Event, err = reconnecting.Marshal()
 
 			default:
@@ -139,12 +153,12 @@ func (s *service) GroupDeviceStatus(req *protocoltypes.GroupDeviceStatus_Request
 			}
 
 			if err != nil {
-				s.logger.Error("unable to handle event status", zap.Error(err))
+				logger.Error("GroupDeviceStatus: unable to handle event", zap.Error(err))
 				continue
 			}
 
 			if err := srv.Send(&evt); err != nil {
-				s.logger.Debug("GroupDeviceStatus: failed to send event", zap.Error(err))
+				logger.Debug("GroupDeviceStatus: failed to send event", zap.Error(err))
 				return err
 			}
 		}
