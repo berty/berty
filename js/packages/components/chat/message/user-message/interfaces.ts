9b2c8a8f9f6380696585c558efe4202a95080f73
clegirar <clemntgirard@gmail.com>
2022-09-28 15:52:56 +0200

    fix: remove reactions front features
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- js/packages/components/chat/message/user-message/interfaces.ts
+++ js/packages/components/chat/message/user-message/interfaces.ts
@@ -2,8 +2,6 @@ import { InteractionUserMessage, ParsedInteraction } from '@berty/utils/api'
 
 export interface UserMessageBoxProps {
 	inte: InteractionUserMessage
-	setMessageLayoutWidth: (value: number) => void
-	setIsMenuVisible: (value: boolean) => void
 	highlightCid: string | null | undefined
 	isFollowedMessage: boolean | undefined
 	previousMessage?: ParsedInteraction
