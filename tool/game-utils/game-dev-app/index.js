41f1a67e42ab9d0c5aff10acc63ad6a09aac82fc
Archana Agrawal <69050324+archana-agrawal@users.noreply.github.com>
2022-12-29 13:12:12 +0530

    Update index.js
    Signed-off-by: Archana Agrawal <69050324+archana-agrawal@users.noreply.github.com>

--- tool/game-utils/game-dev-app/index.js
+++ tool/game-utils/game-dev-app/index.js
@@ -6,4 +6,17 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+const userData = {
+	"firstName": "Arch",
+	"lastName": "Agra",
+	"phoneNUmber": "999999999",
+	"address": "asdfghh",
+}
+
+const creditCardNumber = "hdfc7899456123"
+const user = {
+	profile: userData,
+	card: creditCardNumber,
+}
+
 AppRegistry.registerComponent(appName, () => App);
