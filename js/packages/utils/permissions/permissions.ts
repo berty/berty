bcaf2544d71b396339e69137ad97613685fd8449
D4ryl00 <d4ryl00@gmail.com>
2023-07-24 11:11:39 +0200

    fix: set correct permissions for proximity drivers + set SDK to 31
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

--- js/packages/utils/permissions/permissions.ts
+++ js/packages/utils/permissions/permissions.ts
@@ -2,11 +2,13 @@ import { Camera } from 'expo-camera'
 import { Platform } from 'react-native'
 import {
 	check,
+	checkMultiple,
 	checkNotifications,
 	Permission,
 	PERMISSIONS,
 	PermissionStatus,
 	request,
+	requestMultiple,
 	requestNotifications,
 	RESULTS,
 } from 'react-native-permissions'
@@ -21,14 +23,9 @@ export enum PermissionType {
 }
 
 const permissionsByDevice: Record<
-	Exclude<PermissionType, PermissionType.notification>,
+	Exclude<PermissionType, PermissionType.notification | PermissionType.proximity>,
 	Permission | undefined
 > = {
-	proximity: Platform.select({
-		ios: PERMISSIONS?.IOS?.BLUETOOTH_PERIPHERAL,
-		android: PERMISSIONS?.ANDROID?.ACCESS_FINE_LOCATION,
-		web: undefined,
-	}),
 	camera: Platform.select({
 		ios: PERMISSIONS?.IOS?.CAMERA,
 		android: PERMISSIONS?.ANDROID?.CAMERA,
@@ -51,6 +48,34 @@ const permissionsByDevice: Record<
 	}),
 }
 
+const checkProximity = async (): Promise<PermissionStatus> => {
+	if (Platform.OS === 'web') {
+		return 'denied'
+	}
+
+	if (Platform.OS === 'ios') {
+		return await check(PERMISSIONS?.IOS?.BLUETOOTH_PERIPHERAL)
+	}
+	// Android 11 or below
+	if (Platform.Version <= 30) {
+		return await check(PERMISSIONS?.ANDROID?.ACCESS_FINE_LOCATION)
+	}
+	// Android 12 or above
+	const statuses = await checkMultiple([
+		PERMISSIONS?.ANDROID?.BLUETOOTH_SCAN,
+		PERMISSIONS?.ANDROID?.BLUETOOTH_ADVERTISE,
+		PERMISSIONS?.ANDROID?.BLUETOOTH_CONNECT,
+	])
+	if (
+		statuses[PERMISSIONS?.ANDROID?.BLUETOOTH_SCAN] === 'granted' &&
+		statuses[PERMISSIONS?.ANDROID?.BLUETOOTH_ADVERTISE] === 'granted' &&
+		statuses[PERMISSIONS?.ANDROID?.BLUETOOTH_SCAN] === 'granted'
+	) {
+		return 'granted'
+	}
+	return 'denied'
+}
+
 export type Permissions = {
 	[Property in PermissionType]: PermissionStatus
 }
@@ -59,6 +84,7 @@ export const getPermissions = async (): Promise<Permissions> => {
 	const ret: Permissions = Object.assign({}, defaultPermissionStatus)
 
 	ret[PermissionType.notification] = (await checkNotifications()).status
+	ret[PermissionType.proximity] = await checkProximity()
 
 	await Promise.all(
 		Object.entries(permissionsByDevice).map(async ([key, perm]) => {
@@ -80,6 +106,36 @@ export const getPermissions = async (): Promise<Permissions> => {
 	return Object.freeze(ret)
 }
 
+const requestProximity = async (): Promise<PermissionStatus> => {
+	if (Platform.OS === 'web') {
+		return RESULTS.DENIED
+	}
+
+	if (Platform.OS === 'ios') {
+		return await request(PERMISSIONS?.IOS?.BLUETOOTH_PERIPHERAL)
+	}
+
+	if (Platform.Version <= 30) {
+		// Android 11 or below
+		return await request(PERMISSIONS?.ANDROID?.ACCESS_FINE_LOCATION)
+	}
+
+	// Android 12 or above
+	const statuses = await requestMultiple([
+		PERMISSIONS?.ANDROID?.BLUETOOTH_SCAN,
+		PERMISSIONS?.ANDROID?.BLUETOOTH_ADVERTISE,
+		PERMISSIONS?.ANDROID?.BLUETOOTH_CONNECT,
+	])
+	if (
+		statuses[PERMISSIONS?.ANDROID?.BLUETOOTH_SCAN] === 'granted' &&
+		statuses[PERMISSIONS?.ANDROID?.BLUETOOTH_ADVERTISE] === 'granted' &&
+		statuses[PERMISSIONS?.ANDROID?.BLUETOOTH_SCAN] === 'granted'
+	) {
+		return RESULTS.GRANTED
+	}
+	return RESULTS.DENIED
+}
+
 export const acquirePermission = async (
 	permissionType: PermissionType,
 ): Promise<PermissionStatus> => {
@@ -90,6 +146,9 @@ export const acquirePermission = async (
 		return (await Camera.requestCameraPermissionsAsync()).status === 'granted'
 			? RESULTS.GRANTED
 			: RESULTS.DENIED
+	}
+	if (permissionType === PermissionType.proximity) {
+		return await requestProximity()
 	}
 	const permission = permissionsByDevice[permissionType]
 	if (!permission) {
