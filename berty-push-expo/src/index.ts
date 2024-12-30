940a47e6742a5219adba85d6e932655fa3add833
D4ryl00 <d4ryl00@gmail.com>
2024-12-30 15:11:23 +0100

    chore: rm berty-push-expo
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

--- berty-push-expo/src/index.ts
+++
@@ -1,5 +0,0 @@
-// Reexport the native module. On web, it will be resolved to BertyPushExpoModule.web.ts
-// and on native platforms to BertyPushExpoModule.ts
-export { default } from './BertyPushExpoModule';
-export { default as BertyPushExpoView } from './BertyPushExpoView';
-export * from  './BertyPushExpo.types';
