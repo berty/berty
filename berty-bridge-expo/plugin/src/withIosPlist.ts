b7eb774c4e9164e3e192ecca9640b9175bf50481
D4ryl00 <d4ryl00@gmail.com>
2025-11-21 14:57:36 +0100

    feat: iOS can receive push notifs on background
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

--- berty-bridge-expo/plugin/src/withIosPlist.ts
+++ berty-bridge-expo/plugin/src/withIosPlist.ts
@@ -1,16 +1,60 @@
-import { ConfigPlugin } from "@expo/config-plugins";
+import { ConfigPlugin, withInfoPlist } from "@expo/config-plugins";
 
 const withIosPlist: ConfigPlugin = (config) => {
-	if (!config.ios) {
-		config.ios = {};
-	}
-	if (!config.ios.infoPlist) {
-		config.ios.infoPlist = {};
-	}
+	return withInfoPlist(config, (config) => {
+		if (!config.ios) {
+			config.ios = {};
+		}
+		if (!config.ios.infoPlist) {
+			config.ios.infoPlist = {};
+		}
 
-	config.ios.infoPlist["appGroupID"] = "group.tech.berty";
+		config.ios.infoPlist["appGroupID"] = "group.tech.berty";
 
-	return config;
+		// background
+
+		if (!Array.isArray(config.modResults.BGTaskSchedulerPermittedIdentifiers)) {
+			config.modResults.BGTaskSchedulerPermittedIdentifiers = [];
+		}
+
+		if (
+			!config.modResults.BGTaskSchedulerPermittedIdentifiers.includes(
+				"tech.berty.ios.task.gobridge-process"
+			)
+		) {
+			config.modResults.BGTaskSchedulerPermittedIdentifiers.push(
+				"tech.berty.ios.task.gobridge-process"
+			);
+		}
+
+		if (
+			!config.modResults.BGTaskSchedulerPermittedIdentifiers.includes(
+				"tech.berty.ios.task.gobridge-refresh"
+			)
+		) {
+			config.modResults.BGTaskSchedulerPermittedIdentifiers.push(
+				"tech.berty.ios.task.gobridge-refresh"
+			);
+		}
+
+		if (!Array.isArray(config.modResults.UIBackgroundModes)) {
+			config.modResults.UIBackgroundModes = [];
+		}
+
+		if (!config.modResults.UIBackgroundModes.includes("fetch")) {
+			config.modResults.UIBackgroundModes.push("fetch");
+		}
+
+		if (!config.modResults.UIBackgroundModes.includes("processing")) {
+			config.modResults.UIBackgroundModes.push("processing");
+		}
+
+		if (!config.modResults.UIBackgroundModes.includes("remote-notification")) {
+			config.modResults.UIBackgroundModes.push("remote-notification");
+		}
+
+		return config;
+	});
 };
 
 export default withIosPlist;
